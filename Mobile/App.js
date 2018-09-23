10793eac5d97709577085097c1d270adc44d2ce0
Jacob Jaffe <jacob.jaffe@tufts.edu>
2018-09-23 03:00:48 -0400

    added login flow

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {  createStackNavigator, createSwitchNavigator, createBottomTabNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-      </View>
-    );
+// These are the screens we want to navigate between!
+// Group screens together in file structure when appropriate!
+
+import FooScreen from './src/FooScreen';
+import BarScreen from './src/BarScreen'
+
+import LoginScreen from './src/LoginStack/LoginScreen'
+import SplashScreen from './src/LoginStack/SplashScreen'
+import SignupScreen from './src/LoginStack/SignupScreen'
+
+// This file should just set up navigation, so all actual content is in src/
+// Define what views / tabs / stacks the navigator will use
+
+const _AppTabs = createBottomTabNavigator(
+  {
+    Foo : { screen: FooScreen },
+    Bar : { screen: BarScreen },
   }
-}
+)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+// this probably won't need to be a full stack
+const _LoginStack = createStackNavigator(
+  {
+    Splash: { screen: SplashScreen },
+    Login: { screen: LoginScreen },
+    Signup: { screen: SignupScreen },
   },
-});
+  {
+    initialRouteName: 'Splash',
+  }
+)
+
+export default createSwitchNavigator(
+  {
+    AppSwitch: _AppTabs,
+    LoginSwitch: _LoginStack,
+  },
+  {
+    initialRouteName: 'LoginSwitch',
+  },
+
+)
