19754ad1712f020b49502705ba8894cbd5e3352b
Max Greenwald <maxgreenwald@me.com>
2018-10-06 17:36:51 -0400

    Add email verification

--- src/server/api/auth/index.js
+++ src/server/api/auth/index.js
@@ -4,11 +4,30 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const request = require('request');
+const crypto = require('crypto');
+const sgMail = require('@sendgrid/mail');
 
 const db = require('../../db');
 
 // Initialize the authentication router: /api/auth
 const authRouter = express.Router();
+
+authRouter.get('/verify/:hash', async (req, res) => {
+  const { hash } = req.params;
+
+  const result = await db.query(
+    'UPDATE users SET verified = true WHERE verified = false AND verification_hash = $1 AND verification_expire_date > $2 RETURNING id',
+    [hash, new Date()],
+  );
+
+  // Not verified - no rows were updated
+  if (result.rows.length === 0) {
+    return res.redirect('/not-verified');
+  }
+
+  // Success! Verified
+  return res.redirect('/verified');
+});
 
 authRouter.post('/login', async (req, res) => {
   try {
@@ -28,7 +47,7 @@ authRouter.post('/login', async (req, res) => {
     }
 
     const token = jwt.sign({ id: user.id }, config.secret, {
-      expiresIn: 86400, // expires in 24 hours
+      expiresIn: 31540000, // expires in 24 hours
     });
     return res.status(200).send({ auth: true, token });
   } catch (err) {
@@ -36,7 +55,9 @@ authRouter.post('/login', async (req, res) => {
   }
 });
 
-const post = (urlFormData) => {
+// TODO: Add to a utils function
+/* eslint-disable arrow-body-style */
+const postForm = (urlFormData) => {
   return new Promise((resolve, reject) => {
     request.post(urlFormData, (err, httpResponse, body) => {
       if (err) reject();
@@ -54,29 +75,39 @@ const get = (url) => {
   });
 };
 
+/* eslint-enable arrow-body-style */
+
 authRouter.post('/register', async (req, res) => {
-  const hashedPassword = bcrypt.hashSync(req.body.password, 8);
 
   try {
+    const { utln } = req.body;
+
     // Check Tufts website for proper email
-    const { httpResponse } = await post({ url: 'https://whitepages.tufts.edu/searchresults.cgi', form: { type: 'Students', search: req.body.utln } });
+    const { httpResponse } = await postForm({ url: 'https://whitepages.tufts.edu/searchresults.cgi', form: { type: 'Students', search: utln } });
     const response = await get(`https://whitepages.tufts.edu/${httpResponse.headers.location}`);
     const year = response.split('<b>Class Year: </b>')[1].split('</td></div><td>')[1].split('</td></tr><tr><td>')[0].trim();
-    console.log(year);
+
     if (year !== '19') {
       throw new Error('Could not register user: not in class of 2019');
     }
+
+    const verificationHash = crypto.randomBytes(20).toString('hex');
+    const now = new Date();
+    const expirationDate = new Date(now.getTime() + (10 * 60000)); // 10 minutes from now
+    const hashedPassword = bcrypt.hashSync(req.body.password, 8);
+
     const result = await db.query(
-      'INSERT INTO users(first_name, last_name, email, password) VALUES($1, $2, $3, $4) RETURNING id',
-      [req.body.firstName, req.body.lastName, req.body.email, hashedPassword]
+      'INSERT INTO users(utln, password, verification_hash, verification_expire_date) VALUES($1, $2, $3, $4) RETURNING id',
+      [utln, hashedPassword, verificationHash, expirationDate],
     );
 
-    const token = 'test';/* jwt.sign({ id: result.rows[0].id }, config.get('secret'), {
-      expiresIn: 86400, // expires in 24 hours
-    });*/
-    return res.status(200).send({ auth: true, token });
+    const verificationURL = `http://${req.get('host')}/api/auth/verify/${verificationHash}`;
+    console.log(verificationURL);
+
+    return res.status(200).send({ registered: true, expiration: expirationDate });
   } catch (err) {
     console.log(err);
+    // TODO: Log this to a standard logger
     return res.status(500).send('There was a problem registering the user.');
   }
 });
