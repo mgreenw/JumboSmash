6b3fae3d4aa5e985a43e5bd4e023452fd209193a
Max Greenwald <maxgreenwald@me.com>
2018-10-06 13:21:43 -0400

    Add login and register endpoints

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -6,39 +6,31 @@ const authRouter = require('./auth');
 
 const apiRouter = express.Router();
 const isAuthenticated = (req, res, next) => {
-  const token = req.body.token;
+  const { token } = req.body;
   jwt.verify(token, config.get('secret'), async (err, decoded) => {
-    if (err) return next(authFailure(socket));
-    console.log('DECODED:', decoded);
+    if (err) return res.status(401).send({ error: 'Auth failure. Not logged in.' });
     try {
-      let result = await db.query(
+      const result = await db.query(
         'SELECT id from users where id = $1 LIMIT 1',
-        [decoded.id]
+        [decoded.id],
       );
-      console.log('RESULT:', result.rows);
       if (result.rows.length === 0) {
         // No user. Fail.
-        return next(authFailure(socket));
+        return res.status(401).send({ error: 'Auth failure. Not logged in' });
       }
-    } catch (err) {
-      return next(authFailure(socket));
+    } catch (_) {
+      return res.status(401).send({ error: 'Auth failure. Not logged in' });
     }
-    console.log('successful auth!');
+
+    // Successful Auth!
     return next();
   });
+};
 
-  // CHECK THE USER STORED IN SESSION FOR A CUSTOM VARIABLE
-  // you can do this however you want with whatever variables you set up
-  if (req.user.authenticated) {
-    return next();
-  }
-
-  // IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM SOMEWHERE
-  return res.redirect('/');
-}
-
-apiRouter.use(isAuthenticated);
+// The order here is important
 apiRouter.use('/auth', authRouter);
 
+// All authenticated endpoints must come after this next line
+apiRouter.use(isAuthenticated);
 
 module.exports = apiRouter;
