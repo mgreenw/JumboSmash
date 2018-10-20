1aeaf4136572caa2179556d2f22292e4eddcd243
Jacob Jaffe <jacob.jaffe@tufts.edu>
2018-10-20 15:22:53 -0500

    cleanup

--- src/mobile/reducers/index.js
+++ src/mobile/reducers/index.js
@@ -1,10 +1,11 @@
 // @flow
+import { AsyncStorage } from 'react-native';
+import _ from 'lodash';
 
+// Types:
 import {
   LOGIN_WITH_NEW_TOKEN,
 } from '../actions/auth/login.js';
-
-import _ from 'lodash';
 
 // TODO: seperate state into profile, meta, API responses, etc.
 type State = {
@@ -22,7 +23,11 @@ const defaultState: State = {
 export default function rootReducer(state: State = defaultState, action: any) {
   switch(action.type) {
     case LOGIN_WITH_NEW_TOKEN: {
-      console.log('REDUCER', action);
+
+      // TODO: store to async
+      // https://reactnavigation.org/docs/en/auth-flow.html
+      // https://medium.com/building-with-react-native/what-is-asyncstorage-in-react-native-and-how-you-to-use-it-with-app-state-manager-1x09-b8c636ce5f6e
+
       return _.assign({}, state, {
         utln: action.utln,
         token: action.token,
