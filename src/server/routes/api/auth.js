183ac2566ed5f6219efd06d13f6f738bc8a8821b
Max Greenwald <maxgreenwald@me.com>
2018-10-22 18:38:54 -0400

    Server: remove trailing slash on verify route

--- src/server/routes/api/auth.js
+++ src/server/routes/api/auth.js
@@ -8,6 +8,6 @@ const authRouter = express.Router();
 
 authRouter.post('/check-token-valid', authController.checkTokenValid);
 authRouter.post('/send-verification-email', authController.sendVerificationEmail);
-authRouter.post('/verify/', authController.verify);
+authRouter.post('/verify', authController.verify);
 
 module.exports = authRouter;
