e3846f68a2ae4c590420d8cddd46c73a2d9783df
Max Greenwald <maxgreenwald@me.com>
2018-10-21 23:42:07 -0400

    Server: add /api/auth/check-token-valid endpoint

--- src/server/controllers/auth/utils/index.js
+++ src/server/controllers/auth/utils/index.js
@@ -1,6 +1,34 @@
 // @flow
 
 const request = require('request');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const db = require('../../../db');
+
+function checkAuthenticated(token: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, config.get('secret'), async (err, decoded) => {
+      if (err) return reject();
+      try {
+        const result = await db.query(
+          'SELECT utln FROM users WHERE id = $1 LIMIT 1',
+          [decoded.id],
+        );
+
+        // If no user exists, fail
+        if (result.rowCount === 0) {
+          return reject();
+        }
+
+        // Resolve with the user's utln!
+        return resolve(result.rows[0].utln);
+      } catch (_) {
+        return reject();
+      }
+    });
+  });
+}
 
 /* eslint-disable arrow-body-style */
 function postForm(urlFormData: Object): Promise<any> {
@@ -22,7 +50,15 @@ function get(url: string): Promise<any> {
 }
 /* eslint-enable arrow-body-style */
 
+function verificationCodeExpired(expiration: Date, attempts: number) {
+  // Check the expiration date and the number of attempts on this code.
+  const expired = new Date(expiration).getTime() < new Date().getTime();
+  return expired || attempts >= 3;
+}
+
 module.exports = {
   get,
   postForm,
+  verificationCodeExpired,
+  checkAuthenticated,
 };
--- src/server/routes/api/auth.js
+++ src/server/routes/api/auth.js
@@ -6,6 +6,7 @@ const authController = require('../../controllers/auth');
 
 const authRouter = express.Router();
 
+authRouter.post('/check-token-valid', authController.checkTokenValid);
 authRouter.post('/send-verification-email', authController.sendVerificationEmail);
 authRouter.post('/verify/', authController.verify);
 
