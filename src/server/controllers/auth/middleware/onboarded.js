bb435926e00054c1356bb6b6aa9cebaa3dcfbf55
Max Greenwald <maxgreenwald@me.com>
2018-10-27 14:43:02 -0400

    Server: add onboarding middleware and documentation

--- src/server/controllers/auth/middleware/onboarded.js
+++ src/server/controllers/auth/middleware/onboarded.js
@@ -2,18 +2,28 @@
 
 import type { $Request, $Response, $Next } from 'express';
 
+const utils = require('../../utils');
 const codes = require('../../status-codes');
 
 // Middleware to check if the user is authenticated
 const onboarded = async (req: $Request, res: $Response, next: $Next) => {
-  const { id } = req.user;
-  if (id === undefined) {
-    return res.status(403).json({
-      status: codes.ONBOARDING_INCOMPLETE,
-    });
-  }
+  try {
+    // If the request's user id is undefined, the user is not in the users
+    // table yet, which implies that they are not onboarded.
+    const { id } = req.user;
+    if (id === undefined) {
+      return res.status(403).json({
+        status: codes.ONBOARDING_INCOMPLETE,
+      });
+    }
 
-  return next();
+    // If the user's id is defined, go to the next route!
+    return next();
+  } catch (_) {
+    // Server error. Ensure that the 'authenticated' middleware comes before
+    // the 'onboarded' middleware
+    return utils.error.server(res, 'No user found. Perhaps the user authentication check has not yet occured.');
+  }
 };
 
 module.exports = onboarded;
--- src/server/routes/api/index.js
+++ src/server/routes/api/index.js
@@ -8,18 +8,28 @@ const apiRouter = express.Router();
 const authenticated = require('../../controllers/auth/middleware/authenticated');
 const onboarded = require('../../controllers/auth/middleware/onboarded');
 
-// The order here is important
+
+// API ROUTES
+
+// --> Public Routers <--
+// All routers that require some level of public access must be in this section
+// If they want to include more specificity on which routes require
+// authentication or onboarding, they may use the middleware directly.
 apiRouter.use('/auth', authRouter);
 
-// Authenticated: a user has verified that they are the user for that utln
-// Onboarded: the user has gone through the required profile setup.
-apiRouter.use(authenticated);
-apiRouter.use(onboarded);
 
-apiRouter.get('/ping', (req, res) => {
-  res.send(200).json({
-    status: 'woo',
-  });
-});
+// --> Authenticated Routers <--
+apiRouter.use(authenticated);
+// Authenticated: a user has verified that they are the user for that utln
+// All routers that require some level of public access must be in this section
+// If they want to include more specificity on which routes require
+// authentication or onboarding, they may use the middleware directly.
+
+
+// --> Onboarded Routers <--
+apiRouter.use(onboarded);
+// Onboarded: the user has gone through the required profile setup.
+// Any router for which every route requires the user to be onboarded should
+// go in this section
 
 module.exports = apiRouter;
