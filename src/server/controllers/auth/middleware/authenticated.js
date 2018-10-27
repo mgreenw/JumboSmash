e6e75604549e01571aa7c2fa20ba1bbbe92cd7fa
Max Greenwald <maxgreenwald@me.com>
2018-10-27 16:52:30 -0400

    Server: add check for auth token being included in the request

--- src/server/controllers/auth/middleware/authenticated.js
+++ src/server/controllers/auth/middleware/authenticated.js
@@ -7,13 +7,25 @@ const codes = require('../../status-codes');
 
 // Middleware to check if the user is authenticated
 const authenticated = async (req: $Request, res: $Response, next: $Next) => {
+  // Get the auth token. If it does not exist, return a bad request.
   const token = req.get('Authorization');
+  if (token === undefined) {
+    return res.status(400).json({
+      status: codes.BAD_REQUEST,
+      message: 'Missing Authorization header.',
+    });
+  }
+
   try {
-    // Set the request's "user" property to be the user's id and utln
+    // Set the request's "user" property to be the user object (user
+    // and profile if the user has setup their profile)
     req.user = await authUtils.getUser(token);
 
     // Go to the next middleware
     return next();
+
+  // If there is an error getting the user (like the user does not exist),
+  // return with UNAUTHORIZED
   } catch (error) {
     return res.status(401).json({
       status: codes.UNAUTHORIZED,
--- src/server/controllers/auth/utils/index.js
+++ src/server/controllers/auth/utils/index.js
@@ -11,6 +11,8 @@ function getUser(token: string): Promise<any> {
     jwt.verify(token, config.get('secret'), async (err, decoded) => {
       if (err) return reject();
       try {
+        // Get the user from the users table. If the user has a profile setup,
+        // join with that profile. If not, the 'profile' field will be null
         const result = await db.query(`
           SELECT u.id, u.utln, u.email, p.id AS profile, p.display_name, p.birthday, p.want_he, p.want_she, p.want_they, p.use_he, p.use_she, p.use_they, p.image1_url, p.image2_url, p.image3_url, p.image4_url, p.bio
           FROM users u
@@ -19,12 +21,15 @@ function getUser(token: string): Promise<any> {
           LIMIT 1`,
         [decoded.id]);
 
-        // If no user exists, fail
+        // If no user exists with that id, fail
         if (result.rowCount === 0) {
           return reject();
         }
 
+        // If a user exists, return the user!
         return resolve(result.rows[0]);
+
+      // If there is an unknown error, reject
       } catch (error) {
         return reject();
       }
@@ -58,14 +63,10 @@ function verificationCodeExpired(expiration: Date, attempts: number) {
   return expired || attempts >= 3;
 }
 
-function cleanUtln(utln: string): string {
-  return utln.trim().toLowerCase();
-}
 
 module.exports = {
   get,
   postForm,
   verificationCodeExpired,
   getUser,
-  cleanUtln,
 };
