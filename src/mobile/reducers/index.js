60114cb6a7ad80b4215e40e4850b82d170e8b54c
Jacob Jaffe <jacob.jaffe@tufts.edu>
2018-10-31 14:23:08 -0400

    prettier cleanup

--- src/mobile/reducers/index.js
+++ src/mobile/reducers/index.js
@@ -1,40 +1,38 @@
 // @flow
-import { AsyncStorage } from 'react-native';
-import _ from 'lodash';
+import { AsyncStorage } from "react-native";
+import _ from "lodash";
 
 // Types:
-import {
-  LOGIN_WITH_NEW_TOKEN,
-} from '../actions/auth/login.js';
+import { LOGIN_WITH_NEW_TOKEN } from "../actions/auth/login.js";
 
 // TODO: seperate state into profile, meta, API responses, etc.
 type State = {
   utln: string,
   loggedIn: boolean,
   token: ?string
-}
+};
 
 const defaultState: State = {
-  utln: '',
+  utln: "",
   token: null,
-  loggedIn: false,
-}
+  loggedIn: false
+};
 
 export default function rootReducer(state: State = defaultState, action: any) {
-  switch(action.type) {
+  switch (action.type) {
     case LOGIN_WITH_NEW_TOKEN: {
-      AsyncStorage.setItem('token', action.token);
-      AsyncStorage.setItem('utln', action.utln);
+      AsyncStorage.setItem("token", action.token);
+      AsyncStorage.setItem("utln", action.utln);
 
       return _.assign({}, state, {
         utln: action.utln,
         token: action.token,
-        loggedIn: true,
-      })
+        loggedIn: true
+      });
     }
 
     default: {
-      return state
+      return state;
     }
   }
 }
