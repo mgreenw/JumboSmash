d37d2eb6a610e1237581a0ef012605bd942ef684
Max Greenwald <maxgreenwald@me.com>
2018-11-03 13:55:35 -0400

    Add users test folder

---
+++ src/server/tests/api/users/create-profile.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const codes = require('../../../controllers/status-codes');
+
+const app = require('../../../app');
+const db = require('../../../db');
+
+  // CREATE_PROFILE__SUCCESS,
+  // CREATE_PROFILE__DISPLAY_NAME_TOO_LONG,
+  // CREATE_PROFILE__BIRTHDAY_NOT_VALID,
+  // CREATE_PROFILE__BIO_TOO_LONG,
+  // CREATE_PROFILE__IMAGE_URL_NOT_VALID,
+  // CREATE_PROFILE__PROFILE_ALREADY_CREATED,
+
+describe('POST api/users/me/profile', () => {
+
+});
