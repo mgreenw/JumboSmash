ec6a3d6d515a018c60b6d8a2f08518b92aca93e0
Dan Katz <dan@upstream.tech>
2018-11-03 13:36:47 -0400

    add resend logic and custom back button action

--- src/mobile/components/Auth/ExpiredCode_Screen.js
+++ src/mobile/components/Auth/ExpiredCode_Screen.js
@@ -4,7 +4,13 @@ import React from "react";
 import { Text, View, KeyboardAvoidingView } from "react-native";
 import { Button } from "react-native-elements";
 import { connect } from "react-redux";
+import {
+  StackActions,
+  NavigationActions,
+  HeaderBackButton
+} from "react-navigation";
 import { styles } from "../../styles/auth";
+import sendVerificationEmail from "../../api/auth/sendVerificationEmail";
 
 type Props = {
   navigation: any
@@ -26,14 +32,111 @@ class ExpiredCodeScreen extends React.Component<Props, State> {
     this.state = {};
   }
 
-  static navigationOptions = {
-    headerStyle: {
-      borderBottomWidth: 0
-    }
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerStyle: {
+        borderBottomWidth: 0
+      },
+      headerLeft: (
+        <HeaderBackButton
+          onPress={() => {
+            const resetAction = StackActions.reset({
+              index: 0,
+              actions: [NavigationActions.navigate({ routeName: "Splash" })]
+            });
+            navigation.dispatch(resetAction);
+          }}
+        />
+      )
+    };
+  };
+
+  _onSuccess = (utln: string, email: string) => {
+    const { navigate } = this.props.navigation;
+    navigate("Verify", {
+      utln: utln,
+      email: email
+    });
+  };
+
+  _onNot2019 = (classYear: integer) => {
+    const { navigate } = this.props.navigation;
+    navigate("Not2019", {
+      classYear: classYear
+    });
+  };
+
+  _onNotFound = () => {
+    this._utlnInputError("Could not find UTLN");
+  };
+
+  _onError = (error: any) => {
+    this._onNotFound();
   };
 
   _onResend = () => {
-    console.log("will resend");
+    const { navigation } = this.props;
+    const utln = navigation.getParam("utln", "");
+    // First, we validate the UTLN to preliminarily shake it / display errors
+    if (this._validateUtln(utln)) {
+      this.setState({
+        isSubmitting: true,
+        validUtln: true,
+        errorMessageUtln: ""
+      });
+
+      const stopSubmitting = (callBack: any => void) => {
+        this.setState(
+          {
+            isSubmitting: false
+          },
+          callBack()
+        );
+      };
+
+      // Not the best way to do this for the callbacks with parameters, but
+      // marignally better than before. Need to find a better way to do this
+      // with keeping response types.
+      sendVerificationEmail(
+        {
+          utln,
+          forceResend: true
+        },
+        (response, request) =>
+          stopSubmitting(() => {
+            this._onSuccess(request.utln, response.email);
+          }),
+        (response, request) =>
+          stopSubmitting(() => {
+            this._onNot2019(response.classYear);
+          }),
+        (response, request) => stopSubmitting(this._onNotFound),
+        (response, request) =>
+          stopSubmitting(() => {
+            this._onSuccess(request.utln, response.email);
+          }),
+        (error, request) =>
+          stopSubmitting(() => {
+            this._onError(error);
+          })
+      );
+    }
+  };
+
+  _utlnInputError = (errorMessage: string) => {
+    this.setState({
+      validUtln: false,
+      errorMessageUtln: errorMessage
+    });
+  };
+
+  // TODO: more client side validation!
+  _validateUtln = utln => {
+    if (utln == "") {
+      this._utlnInputError("Required");
+      return false;
+    }
+    return true;
   };
 
   render() {
