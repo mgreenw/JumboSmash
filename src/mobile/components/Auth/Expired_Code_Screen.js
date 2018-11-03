4f628dca1ab657a11e06fca4d51a54fd9e7f0d11
Dan Katz <dan@upstream.tech>
2018-11-03 11:47:16 -0400

    add basic expired code screen

---
+++ src/mobile/components/Auth/Expired_Code_Screen.js
@@ -0,0 +1,67 @@
+// @flow
+
+import React from "react";
+import { Text, View, KeyboardAvoidingView } from "react-native";
+import { Button } from "react-native-elements";
+import { connect } from "react-redux";
+import { styles } from "../../styles/auth";
+
+type Props = {
+  navigation: any
+};
+
+type State = {};
+
+function mapStateToProps(state: State, ownProps: Props) {
+  return {};
+}
+
+function mapDispatchToProps(dispatch: State, ownProps: Props) {
+  return {};
+}
+
+class ExpiredCodeScreen extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+
+  static navigationOptions = {
+    headerStyle: {
+      borderBottomWidth: 0
+    }
+  };
+
+  _onResendPressed() {
+    console.log("will resend");
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const email = navigation.getParam("email", "");
+
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <View style={{ flex: 1 }}>
+          <Text style={styles.title}>Expired Verification Code</Text>
+        </View>
+        <View>
+          <Text
+          >{`Your email verification code has expired. To sign in, have a new code sent to ${email}`}</Text>
+        </View>
+        <View style={{ flex: 1, alignSelf: "stretch" }}>
+          <Button
+            buttonStyle={styles.button}
+            onPress={() => this._onResendPressed()}
+            title="Resend Code"
+          />
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ExpiredCodeScreen);
