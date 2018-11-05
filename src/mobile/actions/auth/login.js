656ceb72369b663283a66a9c19218c0625751d4c
Jacob Jaffe <jacob.jaffe@tufts.edu>
2018-11-05 14:33:50 -0500

    devTesting latency

--- src/mobile/actions/auth/login.js
+++ src/mobile/actions/auth/login.js
@@ -1,6 +1,7 @@
 // @flow
 import type { Dispatch } from "redux";
 import { AsyncStorage } from "react-native";
+import DevTesting from "../../utils/DevTesting";
 
 export const LOGIN_INITIATED = "LOGIN_INITIATED";
 export const LOGIN_COMPLETED = "LOGIN_COMPLETED";
@@ -23,10 +24,10 @@ function complete(utln: string, token: string) {
 export function login(utln: string, token: string) {
   return function(dispatch: Dispatch) {
     dispatch(initiate());
-    setTimeout(() => {
+    DevTesting.fakeLatency(() => {
       AsyncStorage.multiSet([["utln", utln], ["token", token]]).then(errors => {
         dispatch(complete(utln, token));
       });
-    }, 2000);
+    });
   };
 }
