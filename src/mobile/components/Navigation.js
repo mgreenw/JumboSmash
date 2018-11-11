0c7f2978173b3bc0cfbf0099b91ecd2c520c503c
Dan Katz <danielkatz125@gmail.com>
2018-11-11 13:29:01 -0500

    Dan.mobile.onboarding name age screen (#94)
    * add basic name and age onboarding screen

--- src/mobile/components/Navigation.js
+++ src/mobile/components/Navigation.js
@@ -26,6 +26,7 @@ import Help from "./Auth/Help_Screen";
 
 // OnBoarding Screens
 import OnboardingStart from "./Onboarding/OnboardingStart_Screen";
+import OnboardingNameAge from "./Onboarding/OnboardingNameAge_Screen.js";
 
 // This file should just set up navigation, so all actual content is in /
 // Define what views / tabs / stacks the navigator will use
@@ -87,10 +88,18 @@ const AuthStack = createStackNavigator(
 
 const OnboardingStack = createStackNavigator(
   {
-    OnboardingStart: { screen: OnboardingStart }
+    OnboardingStart: { screen: OnboardingStart },
+    OnboardingNameAge: { screen: OnboardingNameAge }
   },
   {
-    initialRouteName: "OnboardingStart"
+    initialRouteName: "OnboardingStart",
+    navigationOptions: {
+      headerBackTitle: null,
+      headerStyle: {
+        borderBottomWidth: 0
+      },
+      title: "Profile Setup"
+    }
   }
 );
 
