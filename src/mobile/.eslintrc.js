0c2f46ad0a68ff8d0b0ac936d7933845355dd2c6
Jacob Jaffe <jacob.jaffe@tufts.edu>
2018-11-11 13:29:19 -0500

    eslint for frontend (#91)

---
+++ src/mobile/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: ["react", "react-native"],
+  parser: "babel-eslint",
+  env: {
+    "react-native/react-native": true
+  },
+  rules: {
+    "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    // "react-native/no-inline-styles": 2,
+    // "react-native/no-color-literals": 2,
+    "react-native/no-raw-text": 2
+  }
+};
