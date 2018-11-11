3d540533a08d1d7497a13fe98dbba32767230c5e
OliveRubio <emcolwell20967@gmail.com>
2018-11-11 13:02:58 -0500

    Added get-my-settings endpoint.

--- src/server/controllers/users/get-my-settings.js
+++ src/server/controllers/users/get-my-settings.js
@@ -4,6 +4,7 @@ import type { $Request, $Response } from 'express';
 
 const utils = require('../utils');
 const codes = require('../status-codes');
+const db = require('../../db');
 
 /* eslint-disable */
 const schema = {
@@ -18,10 +19,28 @@ const schema = {
  *
  */
 const getMySettings = async (req: $Request, res: $Response) => {
-  return res.status(500).json({
-    status: codes.SERVER_ERROR,
-    message: 'Not implemented.',
-  });
+  // try to get the user's settings from the users table
+  try {
+    const result = await db.query(`
+      SELECT
+        wants_he as "wantsHe",
+        wants_she as "wantsShe",
+        wants_they as "wantsThey",
+        uses_he as "usesHe",
+        uses_she as "usesShe",
+        uses_they as "usesThey"
+      FROM users
+      WHERE id = $1`, [req.user.id]);
+
+    // Can assume the user exists and is in db b/c authenticated upstream
+    const settings = result.rows[0];
+    return res.status(200).json({
+      status: codes.GET_SETTINGS__SUCCESS,
+      settings,
+    });
+  } catch (error) {
+    return utils.error.server(res, 'Failed to get user settings.');
+  }
 };
 
 module.exports = [utils.validate(schema), getMySettings];
