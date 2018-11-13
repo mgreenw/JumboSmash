ffd047367daee7d2e41b5afd5df784e20773af5c
Jacob Jaffe <jacob.jaffe@tufts.edu>
2018-11-12 23:32:00 -0500

    Loading App screen + Animation

--- src/mobile/components/App/AppLoading_Screen.js
+++ src/mobile/components/App/AppLoading_Screen.js
@@ -6,14 +6,14 @@ import { Font } from "expo";
 import { connect } from "react-redux";
 import type { Dispatch } from "redux";
 import type { ReduxState } from "mobile/reducers/index";
+import { Colors, Arthur_Styles } from "mobile/styles/Arthur_Styles";
+import ProgressBar from "react-native-progress/Bar";
 
 type Props = {
   navigation: any
 };
 
-type State = {
-  appLoaded: boolean
-};
+type State = {};
 
 function mapStateToProps(reduxState: ReduxState, ownProps: Props) {
   return {};
@@ -29,34 +29,18 @@ function mapDispatchToProps(dispatch: Dispatch, ownProps: Props) {
 class AppLoadingScreen extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = {
-      appLoaded: false
-    };
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    // TODO: this should really be props once we have this through Redux
-    if (this.state.appLoaded && this.state.appLoaded != prevState.appLoaded) {
+    this.state = {};
+    setTimeout(() => {
       const { navigate } = this.props.navigation;
       navigate("Main", {});
-    }
+    }, 2000);
   }
 
   render() {
     return (
       <View style={{ flex: 1 }}>
         <View style={{ flex: 1, justifyContent: "center" }}>
-          <Text
-            style={{
-              color: "#ff6262",
-              fontFamily: "vegan",
-              fontSize: 44,
-              padding: 15,
-              textAlign: "center"
-            }}
-          >
-            Project Gem
-          </Text>
+          <Text style={Arthur_Styles.title}>Project Gem</Text>
         </View>
 
         <View style={{ flex: 1 }}>
@@ -68,9 +52,28 @@ class AppLoadingScreen extends React.Component<Props, State> {
               height: null
             }}
             source={require("../../assets/arthurIcon.png")} // TODO: investigate why  mobile/ does not work
-          />{" "}
+          />
         </View>
-        <View style={{ flex: 1 }} />
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "stretch",
+            paddingLeft: 60,
+            paddingRight: 60
+          }}
+        >
+          <ProgressBar
+            progress={0.3}
+            height={10}
+            unfilledColor={Colors.IceBlue}
+            borderWidth={0}
+            color={Colors.Grapefruit}
+            indeterminate={true}
+            borderRadius={6}
+            width={null}
+          />
+        </View>
       </View>
     );
   }
