4050ef7c7bf06d274ad24877003be8e066b7336c
Jacob Jaffe <jacob.jaffe@tufts.edu>
2018-11-13 17:19:37 -0500

    3. App Load Animation (#105)
    * added colors to style sheet
    
    * navigation overhaul
    
    * app loading page
    
    * cleanup
    
    * Merge branch 'jacob.mobile.colors' into jacob.mobile.veganHeaders
    
    * Loading App screen + Animation
    
    * comments

--- src/mobile/components/Navigation.js
+++ src/mobile/components/Navigation.js
@@ -24,6 +24,8 @@ import Help from "mobile/components/Auth/Login/Help_Screen";
 // APP:
 /////////
 
+import AppLoading from "mobile/components/App/AppLoading_Screen";
+
 // Main App Screens
 import Profile from "mobile/components/App/Main/Profile/Profile_Screen";
 import ProfileEdit from "mobile/components/App/Main/Profile/ProfileEdit_Screen";
@@ -98,7 +100,7 @@ const LoginStack = createStackNavigator(
 const AuthSwitch = createSwitchNavigator(
   {
     Login: LoginStack,
-    AuthLoading: AuthLoading
+    AuthLoading: { screen: AuthLoading }
   },
   {
     initialRouteName: "AuthLoading"
@@ -126,10 +128,11 @@ const OnboardingStack = createStackNavigator(
 const AppSwitch = createSwitchNavigator(
   {
     Main: MainContentSwitch,
-    OnBoarding: OnboardingStack
+    OnBoarding: OnboardingStack,
+    AppLoading: { screen: AppLoading }
   },
   {
-    initialRouteName: "Main"
+    initialRouteName: "AppLoading"
   }
 );
 
