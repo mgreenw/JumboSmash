5941b91b23d5e2e7bd5cc5a513e357bb3e644c46
Dan Katz <danielkatz125@gmail.com>
2018-11-14 17:37:22 -0500

    Dan.mobile.onboarding add pictures (#111)
    * in progress commit
    
    * create AddPhotos component
    
    * use AddPhotos component
    
    * remove unused imports

---
+++ src/mobile/components/App/Onboarding/OnboardingAddPictures_Screen.js
@@ -0,0 +1,67 @@
+// @flow
+import React from "react";
+import {
+  Dimensions,
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  TextInput
+} from "react-native";
+import { connect } from "react-redux";
+import { Button, Icon, Input } from "react-native-elements";
+import type { Dispatch } from "redux";
+import type { ReduxState } from "mobile/reducers/index";
+import AddPhotos from "mobile/components/shared/AddPhotos";
+import { styles } from "mobile/styles/template";
+
+type Props = {
+  navigation: any
+};
+
+type State = {};
+
+function mapStateToProps(reduxState: ReduxState, ownProps: Props) {
+  return {};
+}
+
+function mapDispatchToProps(dispatch: Dispatch, ownProps: Props) {
+  return {};
+}
+
+class OnboardingAddPicturesScreen extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+
+  _onPressContinue = () => {
+    //TODO: Navigate to next page
+    console.log("pressed continue");
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View
+          style={{
+            width: "100%",
+            height: Dimensions.get("window").width
+          }}
+        >
+          <AddPhotos />
+        </View>
+        <Button
+          onPress={this._onPressContinue}
+          title="Continue"
+          buttonStyle={styles.button}
+        />
+      </View>
+    );
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(OnboardingAddPicturesScreen);
