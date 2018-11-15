7d791dccc2af6442b21feeae65de8edd41e19065
Dan Katz <dan@upstream.tech>
2018-11-14 19:48:11 -0500

    Use shared BioInput component

--- src/mobile/components/App/Onboarding/OnboardingBio_Screen.js
+++ src/mobile/components/App/Onboarding/OnboardingBio_Screen.js
@@ -3,7 +3,9 @@ import React from "react";
 import { Text, View, TextInput } from "react-native";
 import { Button, Input } from "react-native-elements";
 import { connect } from "react-redux";
+import BioInput from "mobile/components/shared/BioInput";
 import { styles } from "mobile/styles/template";
+import { Colors, Arthur_Styles } from "mobile/styles/Arthur_Styles";
 import type { Dispatch } from "redux";
 import type { ReduxState } from "mobile/reducers/index";
 
@@ -39,18 +41,8 @@ class OnboardingBioScreen extends React.Component<Props, State> {
   render() {
     return (
       <View style={styles.container}>
-        <Text
-          style={{
-            fontSize: 34,
-            marginLeft: 22,
-            marginRight: 22,
-            textAlign: "center"
-          }}
-        >
-          About Me
-        </Text>
-        <Input
-          multiline={true}
+        <Text style={Arthur_Styles.onboardingHeader}>About Me</Text>
+        <BioInput
           placeholder="The real Tony Monaco"
           onChangeText={bio => this.setState({ bio })}
           value={this.state.bio}
