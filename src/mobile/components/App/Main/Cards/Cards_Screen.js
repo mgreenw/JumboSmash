ea050c85bdd75c8bbad442fa81557ca6bba96669
Dan Katz <dan@upstream.tech>
2018-11-17 16:26:02 -0500

    basic swipe deck

--- src/mobile/components/App/Main/Cards/Cards_Screen.js
+++ src/mobile/components/App/Main/Cards/Cards_Screen.js
@@ -4,10 +4,11 @@ import React from "react";
 import { Text, View } from "react-native";
 import { connect } from "react-redux";
 import { styles } from "mobile/styles/template";
-import { Button, Icon } from "react-native-elements";
+import { Button, Card, Icon } from "react-native-elements";
 import type { Dispatch } from "redux";
 import type { ReduxState } from "mobile/reducers/index";
-import {routes} from "mobile/components/Navigation";
+import { routes } from "mobile/components/Navigation";
+import Deck from "./Deck";
 
 type Props = {
   navigation: any
@@ -23,13 +24,19 @@ function mapDispatchToProps(dispatch: Dispatch, ownProps: Props) {
   return {};
 }
 
+const DATA = [
+  { id: 1, name: "Dan1" },
+  { id: 2, name: "Dan2" },
+  { id: 3, name: "Dan3" },
+  { id: 4, name: "Dan4" }
+];
+
 class SwipingScreen extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {};
   }
 
-  // These are for react navigation, like header bar and such
   static navigationOptions = ({ navigation }) => {
     return {
       title: "Swiping",
@@ -54,15 +61,53 @@ class SwipingScreen extends React.Component<Props, State> {
     };
   };
 
-  render() {
-    // this is the navigator we passed in from App.js
-    const { navigate } = this.props.navigation;
-
+  renderCard(card) {
     return (
-      <View style={styles.container}>
-        <View style={{ flex: 1 }}>
-          <Text style={styles.title}>PROJECT GEM: SWIPING</Text>
-        </View>
+      <Card
+        containerStyle={{
+          borderRadius: 10,
+          padding: 20
+        }}
+        title={card.name}
+        image={{
+          uri:
+            "https://president.tufts.edu/wp-content/uploads/PresMonaco_Sept2011.jpg"
+        }}
+      />
+    );
+  }
+
+  renderNoMoreCards() {
+    return (
+      <Card
+        containerStyle={{
+          borderRadius: 10,
+          padding: 20
+        }}
+        title="Too picky"
+      />
+    );
+  }
+
+  onSwipeRight(card) {
+    console.log("Card liked: " + card.name);
+  }
+
+  onSwipeLeft(card) {
+    console.log("Card disliked: " + card.name);
+  }
+
+  render() {
+    return (
+      <View>
+        <Deck
+          data={DATA}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+          onSwipeRight={this.onSwipeRight}
+          onSwipeLeft={this.onSwipeLeft}
+          infinite={true}
+        />
       </View>
     );
   }
