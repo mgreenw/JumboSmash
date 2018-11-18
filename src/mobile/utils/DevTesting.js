74161614a63283de6b43f05ce2a9ca00b8ab5543
Jacob Jaffe <jacob.jaffe@tufts.edu>
2018-11-17 21:31:57 -0500

    random latency

--- src/mobile/utils/DevTesting.js
+++ src/mobile/utils/DevTesting.js
@@ -1,13 +1,15 @@
 // @flow
 
-const LATENCY_MS = 2000;
+const LATENCY_MAX_MS = 3000;
+const LATENCY_MIN_MS = 100;
 export default {
   // If in dev mode, add a few seconds of latency to requests so that we can
   // ensure proper handling
   fakeLatency(func: () => void) {
     // $FlowFixMe (__DEV__ will break flow)
     if (__DEV__) {
-      setTimeout(func, LATENCY_MS);
+      let latency = Math.floor(Math.random() * LATENCY_MAX_MS) + LATENCY_MIN_MS;
+      setTimeout(func, latency);
     } else {
       func();
     }
