3185ba6c6bd75ec4518a1a148068b69b08c26b84
Dan Katz <dan@upstream.tech>
2018-11-19 12:46:12 -0500

    different render based on isExpanded prop

--- src/mobile/components/App/Main/Cards/Card.js
+++ src/mobile/components/App/Main/Cards/Card.js
@@ -1,7 +1,15 @@
 // @flow
 
 import React from "react";
-import { Text, View, TouchableWithoutFeedback, Dimensions } from "react-native";
+import {
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  Dimensions
+} from "react-native";
 import { connect } from "react-redux";
 import { styles } from "mobile/styles/template";
 import { Button, Card as RneCard, Icon } from "react-native-elements";
@@ -12,7 +20,8 @@ import type { CardType } from "./Deck";
 type Props = {
   navigation: any,
   card: CardType,
-  isExpanded: boolean
+  isExpanded: boolean,
+  onMinimize: () => void
 };
 
 type State = {};
@@ -31,54 +40,118 @@ const SCREEN_HEIGHT = Dimensions.get("window").height;
 class Card extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = {
-      isExpanded: false
-    };
+    this.state = {};
   }
 
   _renderNotExpanded() {
     const { card } = this.props;
     return (
-      <RneCard
-        containerStyle={{
-          borderRadius: 10,
-          padding: 20
+      <View
+        style={{
+          flex: 1,
+          margin: 20
         }}
-        title={card.name}
-        image={{
-          uri:
-            "https://president.tufts.edu/wp-content/uploads/PresMonaco_Sept2011.jpg"
-        }}
-        imageStyle={{
-          aspectRatio: 1
-        }}
-      />
+      >
+        <View style={{ flex: 2 }}>
+          <Image
+            source={{
+              uri:
+                "https://president.tufts.edu/wp-content/uploads/PresMonaco_Sept2011.jpg"
+            }}
+            style={{
+              aspectRatio: 1,
+              borderRadius: 20
+            }}
+          />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "white",
+            padding: 20,
+            alignItems: "center",
+            marginTop: -30,
+            marginLeft: 20,
+            marginRight: 20,
+            borderRadius: 20,
+            shadowOffset: { width: 1, height: 2 },
+            shadowColor: "black",
+            shadowOpacity: 0.2
+          }}
+        >
+          <Text style={{ fontSize: 28 }}>Dan, 21</Text>
+        </View>
+      </View>
     );
   }
 
   _renderExpanded() {
     const { card } = this.props;
     return (
-      <RneCard
-        containerStyle={{
-          width: SCREEN_WIDTH,
-          height: SCREEN_HEIGHT,
-          margin: 0
+      <ScrollView
+        style={{
+          flex: 1
         }}
-        title={card.name}
-        image={{
-          uri:
-            "https://president.tufts.edu/wp-content/uploads/PresMonaco_Sept2011.jpg"
-        }}
-        imageStyle={{
-          aspectRatio: 1
-        }}
-      />
+        contentInset={{ bottom: 40 }}
+      >
+        <View style={{ flex: 1 }}>
+          <Image
+            source={{
+              uri:
+                "https://president.tufts.edu/wp-content/uploads/PresMonaco_Sept2011.jpg"
+            }}
+            style={{
+              aspectRatio: 1
+            }}
+          />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "white",
+
+            alignItems: "center",
+            marginTop: -10,
+            paddingLeft: 20,
+            paddingRight: 20,
+            borderRadius: 10
+          }}
+        >
+          <View
+            style={{
+              position: "relative",
+              width: "100%",
+              marginTop: 20,
+              marginBottom: 20
+            }}
+          >
+            <Text style={{ fontSize: 28, textAlign: "center" }}>Dan, 21</Text>
+
+            <TouchableHighlight
+              style={{ position: "absolute", right: 20, padding: 5 }}
+              onPress={this.props.onMinimize}
+            >
+              <Text style={{ fontSize: 28 }}>{"<"}</Text>
+            </TouchableHighlight>
+          </View>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque sed vehicula urna, in fringilla ipsum. Aenean non lorem
+            quis lorem sollicitudin consequat. Donec tempor erat in ipsum
+            ornare, eu aliquet orci egestas. Vestibulum a convallis metus. Morbi
+            faucibus in orci quis lacinia. Quisque auctor dictum neque, id
+            finibus odio porta in. Suspendisse eget elementum nisl. Vivamus nec
+            massa at dui porta congue. Cras aliquet nunc et elit sodales
+            viverra. Donec elementum semper scelerisque.
+          </Text>
+        </View>
+      </ScrollView>
     );
   }
 
   render() {
     const { isExpanded } = this.props;
+
     let renderedCard;
     if (isExpanded) {
       renderedCard = this._renderExpanded();
@@ -86,7 +159,7 @@ class Card extends React.Component<Props, State> {
       renderedCard = this._renderNotExpanded();
     }
 
-    return <View>{renderedCard}</View>;
+    return renderedCard;
   }
 }
 
