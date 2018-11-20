a155a7893823591c70dd60e598cd9d033781fda4
Max Greenwald <maxgreenwald@me.com>
2018-11-20 15:21:13 -0500

    Add judge endpoint and related migration

--- src/server/controllers/relationships/index.js
+++ src/server/controllers/relationships/index.js
@@ -2,8 +2,10 @@
 
 const getSceneCandidates = require('./get-scene-candidates');
 const getMatches = require('./get-matches');
+const judge = require('./judge');
 
 module.exports = {
   getSceneCandidates,
   getMatches,
+  judge,
 };
--- src/server/routes/api/relationships.js
+++ src/server/routes/api/relationships.js
@@ -9,5 +9,6 @@ const relationshipsRouter = express.Router();
 // HAS PROFILE ONLY METHODS - defined in /api/index.js
 relationshipsRouter.get('/candidates/:scene', relationshipsController.getSceneCandidates);
 relationshipsRouter.get('/matches', relationshipsController.getMatches);
+relationshipsRouter.post('/judge', relationshipsController.judge);
 
 module.exports = relationshipsRouter;
