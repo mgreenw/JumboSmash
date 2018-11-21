4503af9bab5c98fc4329351c542251be6bc0af49
Max Greenwald <maxgreenwald@me.com>
2018-11-21 13:33:47 -0500

    NEW ENDPOINT! POST /api/relationships/block (#148)
    * Add judge endpoint and related migration
    
    * Add judge docs
    
    * Add judge test outline
    
    * finish judge test suite
    
    * Remove babel/eslint changes that snuck their way into this branch
    
    * Fix .eslintrc file
    
    * Allow users without a profile setup to be judged -- important for allowing users to pre-block other users
    
    * Write block query
    
    * Fix type JUDGE_SUCCESS to JUDGE__SUCCESS
    
    * Fix block endpoint controller bug
    
    * Add block docs
    
    * Add tests for block endpoint
    
    * Fix merge conflicts

--- src/server/controllers/relationships/index.js
+++ src/server/controllers/relationships/index.js
@@ -3,9 +3,11 @@
 const getSceneCandidates = require('./get-scene-candidates');
 const getMatches = require('./get-matches');
 const judge = require('./judge');
+const block = require('./block');
 
 module.exports = {
   getSceneCandidates,
   getMatches,
   judge,
+  block,
 };
