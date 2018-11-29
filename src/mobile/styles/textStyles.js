0b26646e57797648653a670a939bae8ab34a697d
Jacob Jaffe <jacob.jaffe@tufts.edu>
2018-11-29 15:21:48 -0500

    added new fonts and rearanged styling

---
+++ src/mobile/styles/textStyles.js
@@ -0,0 +1,95 @@
+// @flow
+import { StyleSheet } from "react-native";
+import { Colors } from "./colors";
+
+// Keep synced with Zeplin export!
+export const textStyles = StyleSheet.create({
+  headline1Style: {
+    fontFamily: "AvenirNext",
+    fontSize: 96,
+    fontWeight: "200",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    color: Colors.Black
+  },
+  headline2Style: {
+    fontFamily: "AvenirNext",
+    fontSize: 60,
+    fontWeight: "200",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    color: Colors.Black
+  },
+  headline3Style: {
+    fontFamily: "AvenirNext",
+    fontSize: 48,
+    fontWeight: "normal",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    color: Colors.Black
+  },
+  headline4Style: {
+    fontFamily: "AvenirNext",
+    fontSize: 34,
+    fontWeight: "normal",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    color: Colors.Black
+  },
+  headline5Style: {
+    fontFamily: "AvenirNext",
+    fontSize: 24,
+    fontWeight: "normal",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    color: Colors.Black
+  },
+  headline6Style: {
+    fontFamily: "AvenirNext",
+    fontSize: 20,
+    fontWeight: "normal",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    color: Colors.Black
+  },
+  body1Style: {
+    fontFamily: "SourceSansPro",
+    fontSize: 18,
+    fontWeight: "300",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    color: Colors.Black
+  },
+  subtitle1Style: {
+    fontFamily: "SourceSansPro",
+    fontSize: 16,
+    fontWeight: "normal",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    color: Colors.Black
+  },
+  body2Style: {
+    fontFamily: "SourceSansPro",
+    fontSize: 14,
+    fontWeight: "normal",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    color: Colors.Black
+  },
+  subtitle2Style: {
+    fontFamily: "SourceSansPro",
+    fontSize: 14,
+    fontWeight: "normal",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    color: Colors.Black
+  },
+  captionStyle: {
+    fontFamily: "SourceSansPro",
+    fontSize: 12,
+    fontWeight: "normal",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    color: Colors.Black
+  }
+});
