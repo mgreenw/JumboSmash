2d84d25f4bacb7f2b5d3f94c65a0e3791b4e7b1f
Jacob Jaffe <jmjaff@msn.com>
2019-01-02 13:02:08 -1000

    formated to server formating specifications

--- src/server/mail/index.js
+++ src/server/mail/index.js
@@ -1,14 +1,14 @@
 // @flow
 
-const config = require("config");
-const sgMail = require("@sendgrid/mail");
+// const config = require("config");
+// const sgMail = require("@sendgrid/mail");
 
-const logger = require("../logger");
-const utils = require("../utils");
+const logger = require('../logger');
+const utils = require('../utils');
 
-const { IncomingWebhook } = require("@slack/client");
-const url =
-  "https://hooks.slack.com/services/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";
+const { IncomingWebhook } = require('@slack/client');
+
+const url = 'https://hooks.slack.com/services/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';
 const webhook = new IncomingWebhook(url);
 
 const NODE_ENV = utils.getNodeEnv();
@@ -21,7 +21,7 @@ function postVerificationCodeToSlack(content: any) {
 
 // Todo: Add logging for these endpoints
 // If running on production, actually send emails. Otherwise, print to console.
-if (NODE_ENV === "production") {
+if (NODE_ENV === 'production') {
   // We shouldn't be sending arbitrary emails without a whitelist untill we're
   // out of beta. Untill we have a better system, let's disable it for now, and
   // in the meantime, verification codes can be posted to Slack.
@@ -29,13 +29,13 @@ if (NODE_ENV === "production") {
   // sgMail.setApiKey(config.get("sendgrid_api_key"));
   // exports.send = sgMail.send;
   exports.send = postVerificationCodeToSlack;
-} else if (NODE_ENV === "development") {
-  exports.send = output => {
+} else if (NODE_ENV === 'development') {
+  exports.send = (output: any) => {
     postVerificationCodeToSlack(output);
     logger.info(JSON.stringify(output, null, 2));
   };
 } else {
-  exports.send = output => {
+  exports.send = (output: any) => {
     return output;
   };
 }
