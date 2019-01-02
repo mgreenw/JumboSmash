63a102cb8d75d9d62c88273cf815a0334f7de53b
Jacob Jaffe <jmjaff@msn.com>
2019-01-01 15:26:39 -1000

    comments

--- src/mobile/assets/icons/CustomIcon.js
+++ src/mobile/assets/icons/CustomIcon.js
@@ -1,6 +1,10 @@
 // @flow
 import { createIconSetFromFontello } from "@expo/vector-icons";
 import fontelloConfig from "./config.json";
+
+// https://github.com/oblador/react-native-vector-icons
+// Takes our icons, which are a font, a config file, and maps them to an
+// object we can import as an IconSet, which we can then use as icons.
 export default createIconSetFromFontello(
   fontelloConfig,
   "gemicons",
