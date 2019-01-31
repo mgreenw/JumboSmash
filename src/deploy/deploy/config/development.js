7d5ff9b7b08548ddc6f33294ecc0931633e67e2b
Spencer Perry <spencerfarrallperry@gmail.com>
2019-01-30 19:30:28 -0500

    added js config file

---
+++ src/deploy/deploy/config/development.js
@@ -0,0 +1,13 @@
+
+  // production.js
+
+  //const secrets = require('../secrets');
+  const raw = require('config/raw').raw;
+  const secrets = require('../secrets');
+
+
+  module.exports = {
+    // secrets.get('sendgrid_api_key'),
+    "DOCKER_USERNAME": 'sperrys',
+    "DOCKER_PASSWORD": secrets.get("DOCKER_PASSWORD"),
+  };
