a0acc287db23ec5c4849f6c35dadabaefc017ecf
Jacob Jaffe <jacob.jaffe@tufts.edu>
2019-01-31 20:44:48 -0500

    throw fix

--- src/mobile/components/App/Main/Profile/ProfileEdit_Screen.js
+++ src/mobile/components/App/Main/Profile/ProfileEdit_Screen.js
@@ -45,11 +45,7 @@ type State = {
 
 function mapStateToProps(reduxState: ReduxState, ownProps: Props): reduxProps {
   if (!reduxState.client) {
-<<<<<<< HEAD
     throw "Redux Client is null in Profile Edit";
-=======
-    throw "Redux User is null";
->>>>>>> eb271348512caa3362227aba4cda1f72b571a625
   }
   return {
     profile: reduxState.client.profile,
