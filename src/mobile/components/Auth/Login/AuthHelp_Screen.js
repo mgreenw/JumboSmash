38c1edf5cce0d4d5d18f0a06b35d3ee14399afe4
Dan Katz <dan@upstream.tech>
2019-02-03 11:22:00 -0500

    fix button path

--- src/mobile/components/Auth/Login/AuthHelp_Screen.js
+++ src/mobile/components/Auth/Login/AuthHelp_Screen.js
@@ -9,7 +9,7 @@ import { Arthur_Styles } from "mobile/styles/Arthur_Styles";
 import { textStyles } from "mobile/styles/textStyles";
 import { routes } from "mobile/components/Navigation";
 import GEMHeader from "mobile/components/shared/Header";
-import { PrimaryButton } from "mobile/components/shared/PrimaryButton";
+import { PrimaryButton } from "mobile/components/shared/buttons/PrimaryButton";
 import AuthLayout from "mobile/components/Auth/Login/Layout";
 
 type navigationProps = {
