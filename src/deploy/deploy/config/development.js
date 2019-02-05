2620dd14d18427e20a148379e4c4c9982e0787b3
Spencer Perry <Spencer.Perry@tufts.edu>
2019-02-04 23:17:40 -0500

    Spencer.infra.pipeline (#237)
    * updated travis with generic language for static site
    
    * initial commit for deploy component
    
    * updated server travis config to operate on standard postgres port of 5432
    
    * added docker push on success
    
    * added docker secrets parsing file
    
    * added diff line to website, website should not build
    
    * added secrets parsing module
    
    * changed docker build image to only master branch
    
    * link docker binary on host deployment
    
    * added development stack file
    
    * added production stack file
    
    * added docker swarm steps to readme

---
+++ src/deploy/deploy/config/development.js
@@ -0,0 +1,11 @@
+
+  // development.js
+
+  const secrets = require('../secrets');
+
+
+  module.exports = {
+    // secrets.get('sendgrid_api_key'),
+    "DOCKER_USERNAME": 'sperrys',
+    "DOCKER_PASSWORD": secrets.get("DOCKER_PASSWORD")
+  };
---
+++ src/deploy/deploy/logger/index.js
@@ -0,0 +1,44 @@
+// @flow
+
+const winston = require('winston');
+const utils = require('../utils');
+
+const NODE_ENV = utils.getNodeEnv();
+
+// Custom format that puts the timestamp before the message
+const simpleTimestamp = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.printf((info) => {
+    return `${info.timestamp} ${info.level}: ${info.message}`;
+  }),
+);
+
+const logger = winston.createLogger({
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log`
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new winston.transports.File({
+      filename: 'error.log',
+      level: 'error',
+      format: simpleTimestamp,
+    }),
+    new winston.transports.File({
+      filename: 'combined.log',
+      format: simpleTimestamp,
+    }),
+  ],
+});
+
+// Don't log to the console in production.
+if (NODE_ENV !== 'production' && NODE_ENV !== 'testing') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple(),
+    ),
+  }));
+}
+
+module.exports = logger;
