b2c801b136efbd65b205c384111a1538b113a793
Jacob Jaffe <jacob.jaffe@tufts.edu>
2019-02-05 16:43:38 -0500

    App.js -> App.jsx

---
+++ src/mobile/App.jsx
@@ -0,0 +1,47 @@
+// @flow
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createAppContainer } from '@react-navigation/native';
+import { loggerMiddleware } from 'mobile/reduxMiddleware/loggerMiddleware';
+import { tokenMiddleware } from 'mobile/reduxMiddleware/tokenMiddleware';
+import { errorMiddleware } from 'mobile/reduxMiddleware/errorMiddleware';
+import NavigationService from './NavigationService';
+import rootReducer from './reducers';
+import { createRootNavigator } from './components/Navigation';
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunkMiddleware),
+    applyMiddleware(errorMiddleware),
+    applyMiddleware(tokenMiddleware),
+    applyMiddleware(loggerMiddleware),
+  ),
+);
+const TopLevelNavigator = createRootNavigator();
+const AppContainer = createAppContainer(TopLevelNavigator);
+
+type Props = {};
+type State = {};
+
+export default class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <AppContainer
+          ref={(navigatorRef) => {
+            NavigationService.setTopLevelNavigator(navigatorRef);
+          }}
+        />
+      </Provider>
+    );
+  }
+}
