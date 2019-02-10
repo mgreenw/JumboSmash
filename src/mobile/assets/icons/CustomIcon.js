99241b736f76e9b55bdd19629d30c1c18979bf16
Dan Katz <dan@upstream.tech>
2019-02-10 14:53:04 -0500

    fix quotes

--- src/mobile/assets/icons/CustomIcon.js
+++ src/mobile/assets/icons/CustomIcon.js
@@ -1,24 +1,24 @@
 // @flow
 /* eslint-disable */
 
-import { createIconSetFromFontello } from "@expo/vector-icons";
-import fontelloConfig from "./config.json";
+import { createIconSetFromFontello } from '@expo/vector-icons';
+import fontelloConfig from './config.json';
 
 export default createIconSetFromFontello(
   fontelloConfig,
-  "gemicons",
-  "gemicons.ttf"
+  'gemicons',
+  'gemicons.ttf'
 );
 
 // add more from fontello as needed. See "demo.html" of the fontello
 // config if you need to figure out the names.
 export type IconName =
-  | "user"
-  | "message"
-  | "cards"
-  | "back"
-  | "life-ring"
-  | "gear"
-  | "check"
-  | "delete-filled"
-  | "heart-filled";
+  | 'user'
+  | 'message'
+  | 'cards'
+  | 'back'
+  | 'life-ring'
+  | 'gear'
+  | 'check'
+  | 'delete-filled'
+  | 'heart-filled';
