cfdf05de1ffcd461ab16e0ff8ce24c9d608e5b4b
Dan Katz <dan@upstream.tech>
2019-02-10 12:44:02 -0500

    add swipe icons

--- src/mobile/assets/icons/CustomIcon.js
+++ src/mobile/assets/icons/CustomIcon.js
@@ -1,19 +1,25 @@
 // @flow
 /* eslint-disable */
 
-import { createIconSetFromFontello } from '@expo/vector-icons';
-import fontelloConfig from './config.json';
+import { createIconSetFromFontello } from "@expo/vector-icons";
+import fontelloConfig from "./config.json";
 
-export default createIconSetFromFontello(fontelloConfig, 'gemicons', 'gemicons.ttf');
+export default createIconSetFromFontello(
+  fontelloConfig,
+  "gemicons",
+  "gemicons.ttf"
+);
 
 // add more from fontello as needed. See "demo.html" of the fontello
 // config if you need to figure out the names.
 export type IconName =
-  | 'user'
-  | 'message'
-  | 'cards'
-  | 'back'
-  | 'life-ring'
-  | 'gear'
-  | 'check'
-  | 'delete-filled';
+  | "user"
+  | "message"
+  | "cards"
+  | "back"
+  | "life-ring"
+  | "gear"
+  | "check"
+  | "delete-filled"
+  | "icon-heart-filled"
+  | "icon-heart-filled";
