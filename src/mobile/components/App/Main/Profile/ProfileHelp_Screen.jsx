722e9f1df0bba45af242cfb5abc82c4462f193f8
Dan Katz <danielkatz125@gmail.com>
2019-02-12 20:42:50 -0500

    Make settings help page look noice (#309)
    * add styling and linting
    
    * fix flow error

--- src/mobile/components/App/Main/Profile/ProfileHelp_Screen.jsx
+++ src/mobile/components/App/Main/Profile/ProfileHelp_Screen.jsx
@@ -1,49 +1,69 @@
 // @flow
-/* eslint-disable */
 
 import React from 'react';
-import { Text, View } from 'react-native';
-import { Button, Icon } from 'react-native-elements';
-import { connect } from 'react-redux';
-import { styles } from 'mobile/styles/auth';
-import type { Dispatch } from 'redux';
-import type { ReduxState } from 'mobile/reducers/index';
-import { Arthur_Styles } from 'mobile/styles/Arthur_Styles';
+import {
+ Text, View, ImageBackground, Linking 
+} from 'react-native';
+import GEMHeader from 'mobile/components/shared/Header';
+import { Colors } from 'mobile/styles/colors';
+import { PrimaryButton } from 'mobile/components/shared/buttons/PrimaryButton';
+import NavigationService from 'mobile/NavigationService';
+import { textStyles } from 'mobile/styles/textStyles';
 
-type Props = {
-  navigation: any,
-};
+const wavesFull = require('../../../../assets/waves/wavesFullScreen/wavesFullScreen.png');
 
-type State = {};
+type Props = {};
 
-function mapStateToProps(reduxState: ReduxState, ownProps: Props) {
-  return {};
-}
-
-function mapDispatchToProps(dispatch: Dispatch, ownProps: Props) {
-  return {};
-}
-
-class ProfileHelpScreen extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-  }
+class ProfileHelp extends React.Component<Props> {
+  _onBack = () => {
+    NavigationService.back();
+  };
 
   render() {
     return (
-      <View style={Arthur_Styles.container}>
-        <Text style={styles.title}>Help & Contact</Text>
-        <Text style={{ textAlign: 'center' }}>NEED HELP?</Text>
-        <Text style={{ textAlign: 'center' }}>
-          {"Send us an email and we'll get back to you the moment our team is sober."}
-        </Text>
-        <Button title="team@jumbosmash.com" buttonStyle={styles.button} />
+      <View style={{ flex: 1 }}>
+        <GEMHeader
+          title="Help & Contact"
+          leftIconName="back"
+          onLeftIconPress={this._onBack}
+        />
+        <View style={{ flex: 1 }}>
+          <ImageBackground
+            source={wavesFull}
+            style={{ width: '100%', height: '100%', position: 'absolute' }}
+          />
+
+          <View
+            style={{
+              backgroundColor: Colors.White,
+              paddingLeft: 44,
+              paddingRight: 44,
+              paddingTop: 20,
+              marginBottom: 20,
+              paddingBottom: 20,
+              marginTop: 20,
+            }}
+          >
+            <View style={{ marginBottom: 30, alignItems: 'center' }}>
+              <Text style={textStyles.headline4StyleDemibold}>NEED HELP?</Text>
+            </View>
+            <View style={{ marginBottom: 30 }}>
+              <Text style={textStyles.body1style}>
+                Send the team an email and they’ll get back to you the moment
+                they’re sober.
+              </Text>
+            </View>
+            <View style={{ marginLeft: 10, marginRight: 10 }}>
+              <PrimaryButton
+                onPress={() => Linking.openURL('mailto:jumbosmash19@gmail.com')}
+                title="support@jumbosmash.com"
+              />
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(ProfileHelpScreen);
+export default ProfileHelp;
