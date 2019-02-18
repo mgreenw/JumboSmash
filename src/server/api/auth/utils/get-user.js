325631e41f57fb7da9999e80f36e0aa0eb3eca01
Max Greenwald <maxgreenwald@me.com>
2019-02-17 21:35:03 -0500

    Fix socket auth middleware not properly catching the result of getUser

--- src/server/api/auth/utils/get-user.js
+++ src/server/api/auth/utils/get-user.js
@@ -4,11 +4,12 @@ const config = require('config');
 const jwt = require('jsonwebtoken');
 
 const db = require('../../../db');
+const logger = require('../../../logger');
 
 function getUser(token: string): Promise<any> {
   return new Promise((resolve, reject) => {
     jwt.verify(token, config.get('secret'), async (err, decoded) => {
-      if (err) return reject();
+      if (err) return reject(err);
       try {
         // Get the user from the users table. If the user has a profile setup,
         // join with that profile. If not, the 'profile' field will be null
@@ -24,20 +25,21 @@ function getUser(token: string): Promise<any> {
 
         // If no user exists with that id, fail
         if (result.rowCount === 0) {
-          return reject();
+          return reject(new Error('User does not exist'));
         }
 
         // Check if the user's token's uuid is valid
         const { tokenUUID, ...user } = result.rows[0];
         if (tokenUUID === null || decoded.uuid !== tokenUUID) {
-          return reject();
+          return reject(new Error('User token invalid'));
         }
         // If a user exists, return the user!
         return resolve(user);
 
         // If there is an unknown error, reject
       } catch (error) {
-        return reject();
+        logger.error('Server Error: Failed to verify user.', error);
+        return reject(error);
       }
     });
   });
