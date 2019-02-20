7ced491e5f190d8914851118d56ae62608215c56
Jacob Jaffe <jacob.jaffe@tufts.edu>
2019-02-20 09:57:57 -0500

    standardize routes with slashes (#351)

--- src/mobile/api/routes.js
+++ src/mobile/api/routes.js
@@ -11,18 +11,18 @@ export const SERVER_ROUTE =
     ? `http://${manifest.debuggerHost
         .split(':')
         .shift()
-        .concat(':3000')}`
-    : 'jumbosmash.com';
+        .concat(':3000/')}`
+    : 'jumbosmash.com/';
 
 // /////////////////////
 // ROUTE CONSTRUCTORS:
 // /////////////////////
 
-const API = '/api';
-const AUTH = '/auth';
-const USERS = '/users';
-const PHOTOS = '/photos';
-const RELATIONSHIPS = '/relationships';
+const API = 'api/';
+const AUTH = 'auth/';
+const USERS = 'users/';
+const PHOTOS = 'photos/';
+const RELATIONSHIPS = 'relationships/';
 
 const AUTH_ROUTE = SERVER_ROUTE + API + AUTH;
 const USERS_ROUTE = SERVER_ROUTE + API + USERS;
@@ -33,42 +33,42 @@ const RELATIONSHIPS_ROUTE = SERVER_ROUTE + API + RELATIONSHIPS;
 // AUTH ROUTES:
 // //////////////
 
-const SEND_VERIFCATION_EMAIL = '/send-verification-email/';
+const SEND_VERIFCATION_EMAIL = 'send-verification-email/';
 export const SEND_VERIFCATION_EMAIL__ROUTE =
   AUTH_ROUTE + SEND_VERIFCATION_EMAIL;
 
-const GET_TOKEN_UTLN = '/get-token-utln/';
+const GET_TOKEN_UTLN = 'get-token-utln/';
 export const GET_TOKEN_UTLN__ROUTE = AUTH_ROUTE + GET_TOKEN_UTLN;
 
-const VERIFY = '/verify/';
+const VERIFY = 'verify/';
 export const VERIFY__ROUTE = AUTH_ROUTE + VERIFY;
 
 // //////////////
 // APP ROUTES:
 // //////////////
-const MY = '/me';
-const PROFILE = '/profile/';
+const MY = 'me/';
+const PROFILE = 'profile/';
 export const MY_PROFILE__ROUTE = USERS_ROUTE + MY + PROFILE;
 
-const SETTINGS = '/settings/';
+const SETTINGS = 'settings/';
 export const MY_SETTINGS__ROUTE = USERS_ROUTE + MY + SETTINGS;
 
 // Photos:
-const SIGN_URL = '/sign-url/';
+const SIGN_URL = 'sign-url/';
 export const GET_SIGN_URL__ROUTE = PHOTOS_ROUTE + SIGN_URL;
-const CONFIRM_PHOTO = '/confirm-upload/';
+const CONFIRM_PHOTO = 'confirm-upload/';
 export const CONFIRM_PHOTO__ROUTE = PHOTOS_ROUTE + CONFIRM_PHOTO;
 
 // My Photos (For onboarding):
-export const GET_MY_PHOTOS__ROUTE = `${USERS_ROUTE + MY}/photos/`;
-export const GET_PHOTO__ROUTE = `${PHOTOS_ROUTE}/`;
-export const DELETE_PHOTO__ROUTE = `${PHOTOS_ROUTE}/`;
+export const GET_MY_PHOTOS__ROUTE = USERS_ROUTE + MY + PHOTOS;
+export const GET_PHOTO__ROUTE = PHOTOS_ROUTE;
+export const DELETE_PHOTO__ROUTE = PHOTOS_ROUTE;
 
 // Candidates:
-const CANDIDATES = '/candidates/';
-const SMASH = 'smash';
-const SOCIAL = 'social';
-const STONE = 'stone';
+const CANDIDATES = 'candidates/';
+const SMASH = 'smash/';
+const SOCIAL = 'social/';
+const STONE = 'stone/';
 const SMASH_CANDIDATES__ROUTE = RELATIONSHIPS_ROUTE + CANDIDATES + SMASH;
 const SOCIAL_CANDIDATES__ROUTE = RELATIONSHIPS_ROUTE + CANDIDATES + SOCIAL;
 const STONE_CANDIDATES__ROUTE = RELATIONSHIPS_ROUTE + CANDIDATES + STONE;
@@ -79,9 +79,9 @@ export const SCENE_CANDIDATES__ROUTES = {
 };
 
 // Matches
-const MATCHES = '/matches/';
+const MATCHES = 'matches/';
 export const GET_MATCHES__ROUTE = RELATIONSHIPS_ROUTE + MATCHES;
 
 // Judge
-const JUDGE = '/judge/';
+const JUDGE = 'judge/';
 export const JUDGE_SCENE_CANDIDATE__ROUTE = RELATIONSHIPS_ROUTE + JUDGE;
