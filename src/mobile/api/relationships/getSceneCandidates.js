9810d34c60a69241de3b0727a15661ea6c9b3a53
Dan Katz <dan@upstream.tech>
2019-02-21 11:37:15 -0500

    add exclude ids as query string

--- src/mobile/api/relationships/getSceneCandidates.js
+++ src/mobile/api/relationships/getSceneCandidates.js
@@ -1,13 +1,21 @@
 // @flow
 
 import type { Candidate, Scene } from 'mobile/reducers';
+import store from 'mobile/store';
+import { arrayToQueryString } from 'mobile/utils/Api';
 import apiRequest from '../utils/apiRequest';
 import { SCENE_CANDIDATES__ROUTES } from '../routes';
 
 const GET_SCENE_CANDIDATES__SUCCESS = 'GET_SCENE_CANDIDATES__SUCCESS';
 
 export default function getSceneCandidates(scene: Scene): Promise<Candidate[]> {
-  return apiRequest('GET', SCENE_CANDIDATES__ROUTES[scene])
+  const { excludeSceneCandidateIds } = store.getState();
+  const queryParams = arrayToQueryString(
+    'exclude[]',
+    excludeSceneCandidateIds[scene]
+  );
+  const queryString = queryParams.length ? `?${queryParams}` : '';
+  return apiRequest('GET', SCENE_CANDIDATES__ROUTES[scene] + queryString)
     .then(response => {
       switch (response.status) {
         case GET_SCENE_CANDIDATES__SUCCESS: {
