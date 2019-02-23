1865b4e4ae10312d0e68cee5c5804a90f1433947
Max Greenwald <maxgreenwald@me.com>
2019-02-23 13:22:10 -0500

    Add conditional aws access key logic

--- src/server/config/production.js
+++ src/server/config/production.js
@@ -13,6 +13,10 @@ module.exports = {
   secret: secrets.get('SECRET'),
   sendgrid_api_key: secrets.get('SENDGRID_API_KEY'),
   koh_host: 'https://koh.jumbosmash.com',
+  aws_credentials: {
+    accessKeyId: secrets.get('AWS_ACCESS_KEY_ID'),
+    secretAccessKey: secrets.get('AWS_SECRET_ACCESS_KEY'),
+  },
   s3_bucket: 'projectgem-prod',
   redis: {
     host: 'gem_redis',
