da51c5d6286868c2ed3a5a0dfb6d2467cb33e97e
Max Greenwald <maxgreenwald@me.com>
2019-02-23 14:51:31 -0500

    Server: Docker Secrets & AWS setup. (#365)
    * Add production.js and secrets files
    
    * Add proper production.js file
    
    * Add conditional aws access key logic
    
    * update prod.yml file
    
    * Add redis network to deploy file
    
    * Fix koh image version
    
    * Fix website typo
    
    * Remove circular import

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const aws = require('aws-sdk');
+const config = require('config');
 
 const logger = require('./logger');
 const api = require('./api');
@@ -11,6 +12,16 @@ const app = express();
 app.use(bodyParser.json());
 
 aws.config.region = 'us-east-1';
+
+// If the aws credentials are already set, don't do anything!
+// If they are not, try to read them from the config file
+// If the config file does not have them, exit.
+if (!aws.config.credentials) {
+  if (!config.has('aws_credentials')) {
+    throw new Error('Could not find AWS credentials. Exiting.');
+  }
+  aws.config.update(config.get('aws_credentials'));
+}
 
 // Log each incoming request
 app.use((req, res, next) => {
