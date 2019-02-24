74c53d5ce0465b75be12f668ccd63ab21d13a36a
Dan Katz <dan@upstream.tech>
2019-02-24 13:14:01 -0500

    add swipe button fade background and padding at bottom of bio scrollview

--- src/mobile/components/App/Main/Cards/SwipeButtons.jsx
+++ src/mobile/components/App/Main/Cards/SwipeButtons.jsx
@@ -1,11 +1,13 @@
 // @flow
 
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
-import { Arthur_Styles } from 'mobile/styles/Arthur_Styles';
+import { View, TouchableOpacity, Dimensions } from 'react-native';
+import { LinearGradient } from 'expo';
 import { Colors } from 'mobile/styles/colors';
 import CustomIcon from 'mobile/assets/icons/CustomIcon';
 import type { SwipeDirection } from './Deck';
+
+const { width } = Dimensions.get('window');
 
 type Props = {
   disabled: boolean,
@@ -15,27 +17,59 @@ type Props = {
 export default (props: Props) => {
   const { onPress, disabled } = props;
   return (
-    <View>
-      <TouchableOpacity
-        onPress={() => onPress('left')}
-        style={Arthur_Styles.swipeButton_dislike}
+    <LinearGradient
+      colors={['rgba(255, 255, 255, 0)', 'rgba(255, 255, 255, 0.7)', 'white']}
+      locations={[0.1, 0.4, 0.7]}
+      style={{
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: 100
+      }}
+    >
+      <View
+        style={{
+          marginBottom: 30,
+          marginHorizontal: width / 4,
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          height: '100%'
+        }}
       >
-        <CustomIcon
-          name="delete-filled"
-          size={65}
-          color={disabled ? 'gray' : 'black'}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => onPress('right')}
-        style={Arthur_Styles.swipeButton_like}
-      >
-        <CustomIcon
-          name="heart-filled"
-          size={65}
-          color={disabled ? 'gray' : Colors.Grapefruit}
-        />
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity
+          onPress={() => onPress('left')}
+          style={{
+            height: 65,
+            width: 65,
+            borderRadius: 65 / 2,
+            backgroundColor: 'white'
+          }}
+        >
+          <CustomIcon
+            name="delete-filled"
+            size={65}
+            color={disabled ? 'gray' : 'black'}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => onPress('right')}
+          style={{
+            height: 65,
+            width: 65,
+            borderRadius: 65 / 2,
+            backgroundColor: 'white'
+          }}
+        >
+          <CustomIcon
+            name="heart-filled"
+            size={65}
+            color={disabled ? 'gray' : Colors.Grapefruit}
+          />
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   );
 };
