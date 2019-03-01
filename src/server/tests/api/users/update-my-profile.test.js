48b765c1f6f1a3eefda171558967b2a76503dccb
Max Greenwald <maxgreenwald@me.com>
2019-02-28 19:13:28 -0500

    Fix logic on invalid secret

--- src/server/tests/api/users/update-my-profile.test.js
+++ src/server/tests/api/users/update-my-profile.test.js
@@ -31,7 +31,7 @@ describe('PATCH api/users/me/profile', () => {
     const res = await request(app)
       .patch('/api/users/me/profile')
       .set('Accept', 'application/json')
-      .set('Authorization', await dbUtils.signToken(1))
+      .set('Authorization', (await dbUtils.signToken(1)).token)
       .send({})
       .expect(401);
     expect(res.body.status).toBe(codes.UNAUTHORIZED.status);
