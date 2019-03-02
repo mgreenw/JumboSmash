71dc1121a2f4aae7cf1643d2c6da869467a90d72
Max Greenwald <maxgreenwald@me.com>
2019-03-02 13:21:44 -0500

    Reinstate testing.json config

--- src/deploy/deploy/config/staging.js
+++ src/deploy/deploy/config/staging.js
@@ -3,7 +3,6 @@
 const secrets = require('../secrets');
 
 module.exports = {
-  // secrets.get('sendgrid_api_key'),
   DOCKER_USERNAME: 'sperrys',
   DOCKER_PASSWORD: secrets.get('DOCKER_PASSWORD'),
 };
