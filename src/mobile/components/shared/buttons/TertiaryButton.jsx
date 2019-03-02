ef3f5619889fd2569f2f35743eca7bf6fb321a5d
Jacob Jaffe <jacob.jaffe@tufts.edu>
2019-03-02 18:45:03 -0500

    help after error (#405)

--- src/mobile/components/shared/buttons/TertiaryButton.jsx
+++ src/mobile/components/shared/buttons/TertiaryButton.jsx
@@ -3,25 +3,38 @@
 import React from 'react';
 import { View, Keyboard, Text } from 'react-native';
 import { textStyles } from 'mobile/styles/textStyles';
+import { Colors } from 'mobile/styles/colors';
 
 type Props = {
   onPress: () => void,
   title: string,
+  hidden?: boolean // we commmonly hide tertiary buttons
 };
 
 export default (props: Props) => {
-  const { onPress, title } = props;
+  const { onPress, title, hidden } = props;
   return (
     <View style={{ height: 18 }}>
       <Text
-        onPress={() => {
-          /* in case a keyboard is up, buttons close them */
-          Keyboard.dismiss();
-          onPress();
-        }}
+        onPress={
+          hidden
+            ? () => {} /* don't perform action if hidden */
+            : () => {
+                /* in case a keyboard is up, buttons close them */
+                Keyboard.dismiss();
+                onPress();
+              }
+        }
         value={title}
         containerStyle={{ width: '100%' }}
-        style={[textStyles.body2Style, { textAlign: 'center', textDecorationLine: 'underline' }]}
+        style={[
+          textStyles.body2Style,
+          {
+            textAlign: 'center',
+            textDecorationLine: 'underline',
+            color: hidden ? 'transparent' : Colors.Black
+          }
+        ]}
       >
         {title}
       </Text>
