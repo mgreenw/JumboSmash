cf6fef77bddfe97d2b0378e908ff4fb7025e34a8
Max Greenwald <maxgreenwald@me.com>
2019-03-04 21:00:07 -0500

    Remove async main

--- src/server/scripts/bump-deploy-versions.js
+++ src/server/scripts/bump-deploy-versions.js
@@ -26,21 +26,19 @@ function updateDeployFile(path, version) {
   fs.writeFileSync(path, env, 'utf8');
 }
 
-// Main function!
-(async () => {
-  const newVersion = semver.parse(process.argv[2]);
-  const prerelease = semver.prerelease(newVersion);
 
-  if (!newVersion) {
-    console.log(`Invalid version ${process.argv[2]}`);
-    process.exit(1);
-  }
+const newVersion = semver.parse(process.argv[2]);
+const prerelease = semver.prerelease(newVersion);
 
-  updateDeployFile('../../deploy/staging.yml', newVersion);
+if (!newVersion) {
+  console.log(`Invalid version ${process.argv[2]}`);
+  process.exit(1);
+}
 
-  if (!prerelease) {
-    updateDeployFile('../../deploy/production.yml', newVersion);
-  }
+updateDeployFile('../../deploy/staging.yml', newVersion);
 
-  console.log('Done!');
-})();
+if (!prerelease) {
+  updateDeployFile('../../deploy/production.yml', newVersion);
+}
+
+console.log('Done!');
