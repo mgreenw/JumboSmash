9cc5ee6860ea4362533a65b220b6a78a536bc221
Max Greenwald <maxgreenwald@me.com>
2019-03-26 20:35:20 -0400

    Add unmatch endpoint

--- src/server/api/relationships/index.js
+++ src/server/api/relationships/index.js
@@ -6,6 +6,7 @@ const getSceneCandidates = require('./get-scene-candidates');
 const getMatches = require('./get-matches');
 const judge = require('./judge');
 const report = require('./report');
+const unmatch = require('./unmatch');
 
 const relationshipsRouter = express.Router();
 
@@ -14,5 +15,6 @@ relationshipsRouter.get('/candidates/:scene', getSceneCandidates.handler);
 relationshipsRouter.get('/matches', getMatches.handler);
 relationshipsRouter.post('/judge', judge.handler);
 relationshipsRouter.post('/report', report.handler);
+relationshipsRouter.post('/unmatch', unmatch.handler);
 
 module.exports = relationshipsRouter;
---
+++ src/server/api/relationships/unmatch.js
@@ -0,0 +1,42 @@
+// @flow
+
+import type { $Request } from 'express';
+
+const { status, asyncHandler, canAccessUserData } = require('../utils');
+const utils = require('./utils');
+const codes = require('../status-codes');
+const db = require('../../db');
+
+const sceneQuery = utils.scenes.map(scene => `liked_${scene} = false, swiped_${scene}_timestamp = NOW()`);
+
+/**
+ * @api {post} /api/relationships/unmatch
+ *
+ */
+const unmatch = async (userId: number, matchUserId: number) => {
+  // If the users are matched in a scene, unmatch them in that scene and require
+  // both users to re-consent to the match
+  const matched = await canAccessUserData(matchUserId, userId, { requireMatch: true });
+  if (!matched) {
+    return status(codes.UNMATCH__NOT_MATCHED).noData();
+  }
+
+  await db.query(`
+    UPDATE relationships
+    SET ${sceneQuery.join(',')}
+    WHERE critic_user_id = $1 AND candidate_user_id = $2
+  `, [userId, matchUserId]);
+
+  return status(codes.UNMATCH__SUCCESS).noData();
+};
+
+const handler = [
+  asyncHandler(async (req: $Request) => {
+    return unmatch(req.user.id, req.params.matchUserId);
+  }),
+];
+
+module.exports = {
+  handler,
+  apply: unmatch,
+};
