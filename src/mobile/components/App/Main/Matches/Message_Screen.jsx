e689f56618fdf3ee713ee7656df4248d667b83f3
Dan Katz <dan@upstream.tech>
2019-03-30 09:51:21 -0400

    make report popup actually report

--- src/mobile/components/App/Main/Matches/Message_Screen.jsx
+++ src/mobile/components/App/Main/Matches/Message_Screen.jsx
@@ -591,7 +591,7 @@ class MessagingScreen extends React.Component<Props, State> {
           )
         }
         displayName={displayName}
-        matchId={match.profile}
+        userId={match.profile}
       />
     );
   }
