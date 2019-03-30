25a693d13cfcd0008da2e8919f86b6d766a89ee1
Max Greenwald <maxgreenwald@me.com>
2019-03-30 14:21:23 -0400

    DB migration for read receipts

--- src/server/db/migrations/1553814895028_relationship-read-receipt.js
+++ src/server/db/migrations/1553814895028_relationship-read-receipt.js
@@ -18,9 +18,81 @@ exports.up = (pgm) => {
     'read_message_id_and_timestamp_both_null_or_exist',
     'CHECK ((num_nulls(critic_read_message_timestamp, critic_read_message_id) = 2) OR (num_nulls(critic_read_message_timestamp, critic_read_message_id) = 0))',
   );
+
+  // Constraints on inserting a read receipt
+  // 1. Once a read receipt exists for a conversation it cannot be removed
+  // 2. A new read receipt's timestamp must be after the previous read receipts timestamp
+  // 3. A new read message must be different from AND sent after the old read message
+  // 4. Only system messages or messages from the critic's match can be read
+  // 5. The read timestamp of the message must be after the sent timestamp of the message
+  pgm.createTrigger('relationships', 'read_message_must_be_from_match_or_system', {
+    when: 'before',
+    operation: ['insert', 'update'],
+    level: 'row',
+    language: 'plpgsql',
+  }, `
+    DECLARE
+      _sender_user_id              integer;
+      _receiver_user_id            integer;
+      _from_system                 boolean;
+      _timestamp                   timestamptz;
+      _last_read_message_timestamp timestamptz;
+    BEGIN
+      IF NEW.critic_read_message_id IS NULL THEN
+        IF TG_OP = 'UPDATE' AND OLD.critic_read_message_id IS NOT NULL THEN
+          RAISE EXCEPTION 'Cannot delete a read receipt once it has been created';
+        END IF;
+
+        /* Because we are not inserting a read receipt, we allow the insertion/update to continue */
+        RETURN NEW;
+      END IF;
+
+      /* Select some data about the message to be read */
+      SELECT
+        sender_user_id, receiver_user_id, from_system, timestamp
+        INTO _sender_user_id, _receiver_user_id, _from_system, _timestamp
+      FROM messages
+      WHERE id = NEW.critic_read_message_id;
+
+      /* A user can only read a system message or a message from the match */
+      IF NOT _from_system AND NEW.candidate_user_id <> _sender_user_id THEN
+        RAISE EXCEPTION 'Only message from the system or from a match % can be read by user %', NEW.candidate_user_id, NEW.critic_user_id;
+      END IF;
+
+      /* If the message can be read, the timestamp must be 1) after the current read timestamp */
+      /* and 2) after the timestamp of the message */
+      IF _timestamp > NEW.critic_read_message_timestamp THEN
+        RAISE EXCEPTION 'The read receipt for a message must be after the message was sent.';
+      END IF;
+
+      /* If this is a not a new relationship, then we must perform additional checks */
+      /* These checks only neeed to be performed if there is already a read receipt */
+      IF TG_OP = 'UPDATE' AND OLD.critic_read_message_id IS NOT NULL THEN
+        IF NEW.critic_read_message_timestamp <= OLD.critic_read_message_timestamp THEN
+          RAISE EXCEPTION 'The new read timestamp must be after the old read timestamp';
+        END IF;
+
+        SELECT timestamp INTO _last_read_message_timestamp
+        FROM messages
+        WHERE id = OLD.critic_read_message_id;
+
+        IF _last_read_message_timestamp > _timestamp THEN
+          RAISE EXCEPTION 'Can only read messages that were sent after the currently read message.';
+        END IF;
+      END IF;
+
+      /* If no exceptions were raised, proceed with the insertion! */
+      RETURN NEW;
+    END
+  `);
 };
 
 exports.down = (pgm) => {
+  pgm.dropTrigger('relationships', 'read_message_must_be_from_match_or_system', {
+    cascade: true,
+    ifExists: true,
+  });
+  pgm.dropFunction('read_message_must_be_from_match_or_system', []);
   pgm.dropConstraint('relationships', 'read_message_id_and_timestamp_both_null_or_exist');
   pgm.dropColumns('relationships', ['critic_read_message_timestamp', 'critic_read_message_id']);
 };
