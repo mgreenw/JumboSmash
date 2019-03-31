7a8973afc78f0373b119353646acc0bbcf5e53e7
Jacob Jaffe <jacob.jaffe@tufts.edu>
2019-03-31 13:26:43 -0400

    own compoenent for spacer

---
+++ src/mobile/components/shared/Spacer.jsx
@@ -0,0 +1,23 @@
+// @flow
+
+import React from 'react';
+import { View } from 'react-native';
+import { Colors } from 'mobile/styles/colors';
+
+const Spacer = () => {
+  return (
+    <View style={{ width: '100%', alignItems: 'center' }}>
+      <View
+        style={{
+          paddingTop: 8,
+          marginBottom: 4,
+          borderTopWidth: 1,
+          width: '80%',
+          borderColor: Colors.Grey80
+        }}
+      />
+    </View>
+  );
+};
+
+export default Spacer;
