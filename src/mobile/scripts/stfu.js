314aaad65c94fb94a6478377230e133aaa378a00
Jacob Jaffe <jacob.jaffe@tufts.edu>
2019-03-31 21:20:19 -0400

    added stfu script

---
+++ src/mobile/scripts/stfu.js
@@ -0,0 +1,11 @@
+// https://github.com/facebook/metro/issues/287
+const fs = require('fs');
+
+const codeToObscure = /console.warn\([\s\S].*"Require cycle: "/;
+const problemFilePath = './node_modules/metro/src/lib/polyfills/require.js';
+const problemFileContent = fs.readFileSync(problemFilePath, 'utf8');
+fs.writeFileSync(
+  problemFilePath,
+  problemFileContent.replace(codeToObscure, 'const noConsoleWarn = (""'),
+  'utf8'
+);
