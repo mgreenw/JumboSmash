ad63a1f644ed1a0d111bcd8f7fbb42b92a0f128d
Jacob Jaffe <jacob.jaffe@tufts.edu>
2019-04-04 10:56:43 -0400

    read message reducer

---
+++ src/mobile/reducers/readMessage.js
@@ -0,0 +1,87 @@
+import type {
+  ReadMessageInitiated_Action,
+  ReadMessageCompleted_Action,
+  ReadMessageFailed_Action
+} from 'mobile/actions/app/readMessage';
+import { ReduxState, ReadMessages, InProgress } from './index';
+
+function updateReadMessages(
+  state: ReduxState,
+  senderUserId: number,
+  messageId: ?number
+): ReadMessages {
+  return {
+    ...state.readMessages,
+    [senderUserId]: messageId
+  };
+}
+
+function updateInProgress(
+  state: ReduxState,
+  senderUserId: number,
+  messageId: ?number,
+  inProgress: boolean
+): InProgress {
+  return {
+    ...inProgress,
+    readMessage: {
+      ...state.inProgress.readMessage,
+      [senderUserId]: {
+        ...state.inProgress.readMessage[senderUserId],
+        [messageId]: inProgress
+      }
+    }
+  };
+}
+
+function initiate(
+  state: ReduxState,
+  action: ReadMessageInitiated_Action
+): ReduxState {
+  const { senderUserId, messageId } = action.payload;
+  const inProgress = updateInProgress(state, senderUserId, messageId, true);
+  return {
+    ...state,
+    inProgress
+  };
+}
+
+function complete(
+  state: ReduxState,
+  action: ReadMessageCompleted_Action
+): ReduxState {
+  const { senderUserId, messageId } = action.payload;
+  const readMessages = updateReadMessages(state, senderUserId, messageId);
+  const inProgress = updateInProgress(state, senderUserId, messageId, false);
+  return {
+    ...state,
+    inProgress,
+    readMessages
+  };
+}
+
+/**
+ *
+ * @param {ReduxState} state
+ * @param {ReadMessageInitiated_Action} action
+ * This is a fire and forget function, so if it fails we just update `inProgress`.
+ * By NOT changing readMessages, this message can still trigger a new readMessage call.
+ */
+function fail(
+  state: ReduxState,
+  action: ReadMessageInitiated_Action
+): ReduxState {
+  const { senderUserId, messageId } = action.payload;
+  const inProgress = updateInProgress(state, senderUserId, messageId, true);
+  return {
+    ...state,
+    inProgress
+  };
+}
+
+export type ReadMessageAction =
+  | ReadMessageInitiated_Action
+  | ReadMessageCompleted_Action
+  | ReadMessageFailed_Action;
+
+export default { initiate, complete, fail };
