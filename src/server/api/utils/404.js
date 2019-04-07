2fab970279fcf8c68e01a32e8195e3772e44be11
Max Greenwald <maxgreenwald@me.com>
2019-04-07 12:21:26 -0400

    Add 404 function

---
+++ src/server/api/utils/404.js
@@ -0,0 +1,12 @@
+// @flow
+
+import type {
+  $Request,
+  $Response,
+  NextFunction,
+  Middleware,
+} from 'express';
+
+module.exports = function notFound(req: $Request, res: $Response, next: NextFunction) {
+
+}
