9c5df30a74b660d0db0f90fb82fcd79dab499c46
Max Greenwald <maxgreenwald@me.com>
2019-04-11 22:14:20 -0400

    1. New Endpoint: Ban User (Admin) (#625)
    * Add admin-password and insert-admina
    
    * Ensure password is at least 8 chars
    
    * Remove old admin tests
    
    * Update canAccessUserData with isAdmin flag
    
    * Update get classmates status
    
    * Add docs for get classmates
    
    * Add test for get classmates
    
    * Return generic 404 on not admin
    
    * Add 404 function
    
    * Update 404 middleware
    
    * Update quote
    
    * Update admin tests to expect 404 instead of 401
    
    * Add additional validation for ban user endpoint
    
    * Add slack hook when a user is banned
    
    * Update ban user tests
    
    * Fix ban user endpoint to only ban the single user
    
    * Include if the user is already banned
    
    * Fix migration to drop view and then recreate it
    
    * Integrate with master

--- src/server/api/admin/index.js
+++ src/server/api/admin/index.js
@@ -4,11 +4,13 @@ const express = require('express');
 
 const { isAdmin } = require('../utils').middleware;
 const getClassmates = require('./get-classmates');
+const banUser = require('./ban-user');
 
 // ADMIN ONLY: All admin routes are authenticated by isAdmin
 const adminRouter = express.Router();
 adminRouter.use(isAdmin);
 
 adminRouter.get('/classmates', getClassmates.handler);
+adminRouter.post('/ban/:userId', banUser.handler);
 
 module.exports = adminRouter;
