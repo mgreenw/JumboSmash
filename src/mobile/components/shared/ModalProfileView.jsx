d6a3135c365ed03d1f22e683c46f113a67fc7194
Jacob Jaffe <jacob.jaffe@tufts.edu>
2019-04-15 19:58:19 -0400

    Update ModalProfileView.jsx

--- src/mobile/components/shared/ModalProfileView.jsx
+++ src/mobile/components/shared/ModalProfileView.jsx
@@ -54,7 +54,7 @@ export default (props: Props) => {
       <AndroidBackHandler
         onBackPress={() => {
           onMinimize();
-          return true;
+          return false;
         }}
       />
     </Modal>
