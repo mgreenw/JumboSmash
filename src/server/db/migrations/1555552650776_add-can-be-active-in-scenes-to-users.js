9e4b173209b68b66d9bbc11f854311b0e65d47ea
Max Greenwald <maxgreenwald@me.com>
2019-04-17 22:00:23 -0400

    Add migration for canbeactiveinscenes to users

---
+++ src/server/db/migrations/1555552650776_add-can-be-active-in-scenes-to-users.js
@@ -0,0 +1,54 @@
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.dropView('users');
+  pgm.createView('users', { replace: true }, `
+    SELECT
+      id,
+      utln,
+      email,
+      successful_logins,
+      want_he,
+      want_she,
+      want_they,
+      use_he,
+      use_she,
+      use_they,
+      active_smash,
+      active_social,
+      active_stone,
+      token_uuid,
+      admin_password,
+      expo_push_token,
+      notifications_enabled,
+      can_be_active_in_scenes
+    FROM classmates
+    WHERE NOT terminated
+  `);
+};
+
+exports.down = (pgm) => {
+  pgm.dropView('users');
+  pgm.createView('users', { replace: true }, `
+    SELECT
+      id,
+      utln,
+      email,
+      successful_logins,
+      want_he,
+      want_she,
+      want_they,
+      use_he,
+      use_she,
+      use_they,
+      active_smash,
+      active_social,
+      active_stone,
+      token_uuid,
+      admin_password,
+      expo_push_token,
+      notifications_enabled
+    FROM classmates
+    WHERE NOT terminated
+  `);
+};
