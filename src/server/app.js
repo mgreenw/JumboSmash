cdfcc102a1bacd21db238fc10ce4b8b9b76e26a0
Max Greenwald <maxgreenwald@me.com>
2019-04-17 21:36:20 -0400

    Initial sentry integration

--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,27 @@
 // @flow
 
 const express = require('express');
+const Sentry = require('@sentry/node');
 
 const logger = require('./logger');
 const api = require('./api');
 const { notFound } = require('./api/utils');
+const codes = require('./api/status-codes');
+const slack = require('./slack');
+const utils = require('./utils');
+const { version } = require('./package.json');
+
+const NODE_ENV = utils.getNodeEnv();
+
+Sentry.init({
+  dsn: 'https://79851436560a4133a55510f62d656e6f@sentry.io/1441637',
+  release: version,
+  environment: NODE_ENV,
+});
 
 const app = express();
+app.use(Sentry.Handlers.requestHandler());
+
 app.use(express.json());
 
 // Log each incoming request
@@ -16,6 +31,11 @@ app.use((req, res, next) => {
     : '';
   const start = new Date().getTime();
   let logged = false;
+
+  Sentry.getHubFromCarrier(req).configureScope((scope) => {
+    scope.setTag('method', req.method);
+    scope.setTag('url', req.originalUrl);
+  });
 
   function logRequest(timeout: boolean = false) {
     if (logged) {
@@ -59,7 +79,24 @@ app.use((req, res, next) => {
 
 // Define all routes here.
 app.use('/api', api);
-
 app.use(notFound);
+
+// --> Main Error Handler! <--
+/* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
+app.use(Sentry.Handlers.errorHandler());
+app.use((err, req, res, _next) => {
+  logger.error('Server Error: ', err);
+  slack.postServerUpdate(`SERVER ERROR
+    Environment: *${NODE_ENV}*
+
+    ${err.message}
+    ${err.stack}
+  `);
+  return res.status(500).json({
+    status: codes.SERVER_ERROR.status,
+    version: utils.version,
+  });
+});
+
 
 module.exports = app;
