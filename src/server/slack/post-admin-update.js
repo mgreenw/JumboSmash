dab99ec088934bc28b69109e4ded2025e2695e46
Max Greenwald <maxgreenwald@me.com>
2019-05-05 19:16:49 -0400

    author on admin slack message

--- src/server/slack/post-admin-update.js
+++ src/server/slack/post-admin-update.js
@@ -4,7 +4,11 @@ const { IncomingWebhook } = require('@slack/client');
 const NODE_ENV = require('../utils').getNodeEnv();
 const { colors, generateUserInfoSection } = require('./utils');
 
-const admin = new IncomingWebhook('https://hooks.slack.com/services/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');
+const route = NODE_ENV === 'development'
+  ? 'https://hooks.slack.com/services/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'
+  : 'https://hooks.slack.com/services/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';
+
+const admin = new IncomingWebhook(route);
 
 type AdminUpdate = 'Terminate User' | 'Review Profile';
 async function postAdminUpdate(
@@ -38,28 +42,13 @@ async function postAdminUpdate(
           color,
           blocks: [
             {
-              type: 'section',
-              fields: [
+              type: 'context',
+              elements: [
                 {
                   type: 'mrkdwn',
-                  text: `*Environment:*\n\`${NODE_ENV}\``,
-                },
-                {
-                  type: 'mrkdwn',
-                  text: `*Type:*\n${type}`,
-                },
-                {
-                  type: 'mrkdwn',
-                  text: `*Admin User Id:*\n${adminUserId}`,
-                },
-                {
-                  type: 'mrkdwn',
-                  text: `*Admin UTLN:*\n${adminUtln}`,
+                  text: `Admin: ${adminUtln} in \`${NODE_ENV}\``,
                 },
               ],
-            },
-            {
-              type: 'divider',
             },
             ...action,
             ...userInfo,
