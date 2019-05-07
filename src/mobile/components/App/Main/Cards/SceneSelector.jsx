db8636a0536b4c88baaa8ac034e8ef05e96527b8
Jacob Jaffe <jacob.jaffe@tufts.edu>
2019-05-07 11:21:14 -0400

    add stone to scene selector

--- src/mobile/components/App/Main/Cards/SceneSelector.jsx
+++ src/mobile/components/App/Main/Cards/SceneSelector.jsx
@@ -6,62 +6,57 @@ import type AnimatedValue from 'react-native/Libraries/Animated/src/nodes/Animat
 import type { Scene } from 'mobile/reducers';
 import { Colors } from 'mobile/styles/colors';
 import NavigationService from 'mobile/components/navigation/NavigationService';
+import { sceneToEmoji } from 'mobile/utils/emojis';
 
 type Props = {
   scene: Scene
 };
 
-const SCENES: { label: string, value: Scene }[] = [
-  { label: '🐘', value: 'social' },
-  { label: '🍑', value: 'smash' }
-];
+const SCENE_ORDER: Scene[] = ['social', 'smash', 'stone'];
+const NUM_SCENES = SCENE_ORDER.length;
+const ICON_WIDTH = 40;
 
-// helper function -- should be made more modular with the Emojis util we have later.
-// for now just a quick hack for sanity.
 function sceneIndex(scene: Scene) {
-  return scene === 'smash' ? 1 : 0;
+  const index = SCENE_ORDER.indexOf(scene);
+  if (index > -1) return index;
+  return 1; // if something goes wrong with props, default to smash.
 }
-
-const WIDTH = 40 * SCENES.length;
 
 export default class SceneSelector extends Component<Props> {
   constructor(props: Props) {
     super(props);
     const startIndex = sceneIndex(props.scene);
     this.selectedSceneHorizontalPosition = new Animated.Value(
-      startIndex / SCENES.length
+      startIndex / NUM_SCENES
     );
   }
 
   toggleItem = (index: number) => {
-    // TOOD: make this better for when we have stone.
-    // for now, an easy hack to switch to the other scene.
-    NavigationService.navigateToCards(SCENES[index].value);
+    NavigationService.navigateToCards(SCENE_ORDER[index]);
   };
 
   selectedSceneHorizontalPosition: AnimatedValue;
 
   render() {
     const { scene: currentScene } = this.props;
-    const iconWidth = WIDTH / SCENES.length;
-    const renderedScenes = SCENES.map((scene, index) => (
+    const renderedScenes = SCENE_ORDER.map((scene, index) => (
       <View
-        key={scene.value}
+        key={scene}
         style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}
       >
         <TouchableOpacity
           style={{
             alignItems: 'center',
-            height: iconWidth,
-            width: iconWidth,
+            height: ICON_WIDTH,
+            width: ICON_WIDTH,
             alignContent: 'center',
             justifyContent: 'center',
             borderRadius: 25,
             borderWidth: 2,
             backgroundColor:
-              scene.value === currentScene ? Colors.White : 'transparent',
+              scene === currentScene ? Colors.White : 'transparent',
             borderColor:
-              scene.value === currentScene ? Colors.AquaMarine : 'transparent'
+              scene === currentScene ? Colors.AquaMarine : 'transparent'
           }}
           onPress={() => this.toggleItem(index)}
         >
@@ -70,7 +65,7 @@ export default class SceneSelector extends Component<Props> {
               fontSize: 20
             }}
           >
-            {scene.label}
+            {sceneToEmoji(scene)}
           </Text>
         </TouchableOpacity>
       </View>
@@ -80,10 +75,10 @@ export default class SceneSelector extends Component<Props> {
       <View
         style={{
           flexDirection: 'row',
-          width: WIDTH,
+          width: ICON_WIDTH * NUM_SCENES,
           borderRadius: 50,
           backgroundColor: Colors.Grey80,
-          height: WIDTH / SCENES.length
+          height: ICON_WIDTH
         }}
       >
         <View
