75a02d260a9bb482900688989df77cf8e5910628
Jacob Jaffe <jacob.jaffe@tufts.edu>
2019-05-09 00:11:21 -0400

    Add release channel to version info for non-prod environments (#906)

---
+++ src/mobile/utils/versionInfo.js
@@ -0,0 +1,22 @@
+// @flow
+import { Constants } from 'expo';
+
+function getReleaseChannel(): string {
+  const { releaseChannel } = Constants.manifest;
+  if (releaseChannel === undefined) return 'development';
+  return releaseChannel;
+}
+
+function getVersion(): string {
+  return Constants.manifest.version;
+}
+
+export default function formattedVersionInfo(): string {
+  const version = getVersion();
+  const releaseChannel = getReleaseChannel();
+  const versionInfo = `Version ${version}`;
+  if (releaseChannel === 'production') {
+    return versionInfo;
+  }
+  return `${versionInfo} (${releaseChannel})`;
+}
