ef74322c489d7380b09f52a56f9e9cca81d16272
Jacob Jaffe <jacob.jaffe@tufts.edu>
2019-05-09 00:10:44 -0400

    (STONE) Inactive Scene Card Emoji Fix (#904)
    * inactive scene card improvements
    
    * Update InactiveSceneCard.jsx

--- src/mobile/components/App/Main/Cards/CardViews/InactiveSceneCard.jsx
+++ src/mobile/components/App/Main/Cards/CardViews/InactiveSceneCard.jsx
@@ -8,25 +8,15 @@ import { PrimaryButton } from 'mobile/components/shared/buttons';
 import type { Scene, ReduxState, Dispatch } from 'mobile/reducers';
 import { connect } from 'react-redux';
 import { enableScene as enableSceneAction } from 'mobile/actions/app/saveSettings';
+import { sceneToEmoji } from 'mobile/utils/emojis';
+import capitalize from 'mobile/utils/Capitalize';
 
-const SCENE_META_DATA = {
-  smash: {
-    display: 'JumboSmash',
-    icon: '🍑',
-    description: 'This is where you can match with people to get ~frisky~'
-  },
-  social: {
-    display: 'JumboSocial',
-    icon: '🐘',
-    description:
-      'This is where you can match with people for hanging out - from study buddies to a night out on the town.'
-  },
-  stone: {
-    display: 'JumboStone',
-    icon: '🍀',
-    description:
-      'This is where you can match with people to get blazed out of your mind'
-  }
+const SCENE_DESCRIPTIONS: { [scene: Scene]: string } = {
+  smash: 'This is where you can match with people to get ~frisky~',
+  social:
+    'This is where you can match with people for hanging out - from study buddies to a night out on the town.',
+  stone:
+    'This is where you can match with people to study geology'
 };
 
 type ProppyProps = {
@@ -89,7 +79,9 @@ class InactiveSceneCard extends React.Component<Props> {
       dismissCard,
       canBeActiveInScenes
     } = this.props;
-    const sceneData = SCENE_META_DATA[scene];
+    const sceneName = `Jumbo${capitalize(scene)}`;
+    const icon = sceneToEmoji(scene);
+    const description = SCENE_DESCRIPTIONS[scene];
 
     return (
       <View
@@ -114,7 +106,7 @@ class InactiveSceneCard extends React.Component<Props> {
               { fontSize: 40, padding: 15, textAlign: 'center' }
             ]}
           >
-            {sceneData.display}
+            {sceneName}
           </Text>
         </View>
         <View
@@ -131,7 +123,7 @@ class InactiveSceneCard extends React.Component<Props> {
               alignItems: 'center'
             }}
           >
-            <Text style={{ fontSize: 69 }}>{sceneData.icon}</Text>
+            <Text style={{ fontSize: 69 }}>{icon}</Text>
           </View>
         </View>
         <View
@@ -142,12 +134,12 @@ class InactiveSceneCard extends React.Component<Props> {
           }}
         >
           <Text style={[textStyles.headline6Style, { textAlign: 'center' }]}>
-            {sceneData.description}
+            {description}
           </Text>
         </View>
         <PrimaryButton
           onPress={sceneEnabled ? dismissCard : enableScene}
-          title={sceneEnabled ? 'Start Swiping' : `Enable ${sceneData.display}`}
+          title={sceneEnabled ? 'Start Swiping' : `Enable ${sceneName}`}
           loading={enableSceneInProgress}
           disabled={enableSceneInProgress || !canBeActiveInScenes}
         />
@@ -165,9 +157,7 @@ class InactiveSceneCard extends React.Component<Props> {
               ]}
             >
               {canBeActiveInScenes
-                ? `You won’t be shown in ${
-                    sceneData.display
-                  } unless you enable it. You can always turn it off in settings.`
+                ? `You won’t be shown in ${sceneName} unless you enable it. You can always turn it off in settings.`
                 : 'Your account is currently locked. Please check your email for more information.'}
             </Text>
           )}
