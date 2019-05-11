12a95de985a21ad020322c91c755540f2ef0f1a7
Max Greenwald <maxgreenwald@me.com>
2019-05-11 03:52:04 -0400

    Cascade vote deletion on yak deletion

--- src/server/db/migrations/1557560489894_cascade-vote-deletion.js
+++ src/server/db/migrations/1557560489894_cascade-vote-deletion.js
@@ -1,11 +1,22 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-  pgm.alterColumn('yak_votes', 'yak_id', {
-    onDelete: 'cascade',
+  pgm.dropConstraint('yak_votes', 'yak_votes_yak_id_fkey');
+  pgm.addConstraint('yak_votes', 'yak_votes_yak_id_fkey', {
+    foreignKeys: {
+      columns: 'yak_id',
+      references: 'yaks',
+      onDelete: 'cascade',
+    },
   });
 };
 
 exports.down = (pgm) => {
-
+  pgm.dropConstraint('yak_votes', 'yak_votes_yak_id_fkey');
+  pgm.addConstraint('yak_votes', 'yak_votes_yak_id_fkey', {
+    foreignKeys: {
+      columns: 'yak_id',
+      references: 'yaks',
+    },
+  });
 };
