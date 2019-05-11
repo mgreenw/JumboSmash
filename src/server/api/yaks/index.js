2f48169d76a1d834a6f9f766b54defb3e2f32405
Max Greenwald <maxgreenwald@me.com>
2019-05-11 03:47:33 -0400

    Y4. Vote On Yak (#922)
    * db migration
    
    * yaks router
    
    * Setup get yaks endpoint
    
    * Add get yaks logic
    
    * Fix sql error
    
    * Remove redis from get yaks
    
    * Add post yak
    
    * unique yak votes per user
    
    * Remove logger
    
    * Add vote on yak error case
    
    * finalize merge

--- src/server/api/yaks/index.js
+++ src/server/api/yaks/index.js
@@ -3,10 +3,12 @@
 const express = require('express');
 const getYaks = require('./get-yaks');
 const postYak = require('./post-yak');
+const voteOnYak = require('./vote-on-yak');
 
 const yaksRouter = express.Router();
 
 yaksRouter.get('/', getYaks.handler);
 yaksRouter.post('/', postYak.handler);
+yaksRouter.patch('/:yakId(\\d+$)', voteOnYak.handler);
 
 module.exports = yaksRouter;
