7ec5402a4c9b387e9bf45881c6198fd26e4d2867
Max Greenwald <maxgreenwald@me.com>
2019-05-11 02:35:58 -0400

    finalize merge

--- src/server/api/yaks/index.js
+++ src/server/api/yaks/index.js
@@ -2,19 +2,13 @@
 
 const express = require('express');
 const getYaks = require('./get-yaks');
-<<<<<<< HEAD
 const postYak = require('./post-yak');
 const voteOnYak = require('./vote-on-yak');
-=======
->>>>>>> master
 
 const yaksRouter = express.Router();
 
 yaksRouter.get('/', getYaks.handler);
-<<<<<<< HEAD
 yaksRouter.post('/', postYak.handler);
 yaksRouter.patch('/:yakId(\\d+$)', voteOnYak.handler);
-=======
->>>>>>> master
 
 module.exports = yaksRouter;
