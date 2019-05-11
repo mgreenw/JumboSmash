955a0d3944c4528934d0f6a03eca01eda4744048
Max Greenwald <maxgreenwald@me.com>
2019-05-11 01:25:53 -0400

    db migration

---
+++ src/server/db/migrations/1557551642311_yak.js
@@ -0,0 +1,54 @@
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('yaks', {
+    id: 'id',
+    user_id: {
+      type: 'integer',
+      references: 'classmates',
+      notNull: true,
+    },
+    content: {
+      type: 'varchar(300)',
+      notNull: true,
+    },
+    timestamp: {
+      type: 'timestamptz',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    score: {
+      type: 'integer',
+      default: 0,
+      notNull: true,
+    },
+  });
+
+  pgm.createTable('yak_votes', {
+    id: 'id',
+    user_id: {
+      type: 'integer',
+      references: 'classmates',
+      notNull: true,
+    },
+    yak_id: {
+      type: 'integer',
+      references: 'classmates',
+      notNull: true,
+    },
+    liked: {
+      type: 'boolean',
+      notNull: true,
+    },
+    updated_timestamp: {
+      type: 'timestamptz',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('yak_votes');
+  pgm.dropTable('yaks');
+};
